180da17d63087332efdec5b80bedcce2dceafc0a
Billy <wwpierce020602@gmail.com>
2017-01-08 13:28:12 -0800

    Added declarer of dump subsystem and added outtake spinning commands.

--- src/org/usfirst/frc4904/robot/subsystems/Dump.java
+++ src/org/usfirst/frc4904/robot/subsystems/Dump.java
@@ -1,16 +1,29 @@
 package org.usfirst.frc4904.robot.subsystems;
 
 
+import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Dump extends Subsystem {
 	
-	public Dump() {
-		
+	SpeedController Elevator;
+	SpeedController Outtake;
+	
+	public Dump(SpeedController e, SpeedController o) { // Public Dump - Let Everyone see it!
+		Elevator = e;
+		Outtake = o;
 	}
 	
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
+	}
+	
+	public void beingSpinOuttake() {
+		Elevator.set(1.0);
+	}
+	
+	public void stopSpinOuttake() {
+		Elevator.set(0.0);
 	}
 }
