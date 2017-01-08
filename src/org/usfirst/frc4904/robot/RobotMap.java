653389e87f874741106fe6e2e562415f72b6780f
Billy <wwpierce020602@gmail.com>
2017-01-08 13:45:05 -0800

    Intialized subsystems (flywheel, ball intake, and ball dumper) in the components

--- src/org/usfirst/frc4904/robot/RobotMap.java
+++ src/org/usfirst/frc4904/robot/RobotMap.java
@@ -1,11 +1,17 @@
 package org.usfirst.frc4904.robot;
 
 
+import org.usfirst.frc4904.robot.subsystems.BallInnie;
+import org.usfirst.frc4904.robot.subsystems.Dump;
+import org.usfirst.frc4904.robot.subsystems.Flywheel;
 import org.usfirst.frc4904.standard.custom.controllers.CustomJoystick;
 import org.usfirst.frc4904.standard.custom.controllers.CustomXbox;
 import org.usfirst.frc4904.standard.custom.motioncontrollers.CustomPIDController;
+import org.usfirst.frc4904.standard.custom.sensors.CANEncoder;
 import org.usfirst.frc4904.standard.custom.sensors.PDP;
 import org.usfirst.frc4904.standard.subsystems.motor.PositionEncodedMotor;
+import org.usfirst.frc4904.standard.subsystems.motor.speedmodifiers.AccelerationCap;
+import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -25,9 +31,9 @@ public class RobotMap {
 			public static final int leftDriveMotor = 0;
 			public static final int rightDriveMotor = 1;
 			public static final int ballInnie = 2;
-			public static final int trayInnie = 3;
-			public static final int flywheelAMotor = 4;
-			public static final int flywheelBMotor = 5;
+			// public static final int trayInnie = 3; ----- PROJECT CURRENTLY PAUSED
+			public static final int flywheelLeftMotor = 4;
+			public static final int flywheelRightMotor = 5;
 			public static final int vomitElevator = 6;
 			public static final int vomitOuttakeRoller = 7;
 		}
@@ -79,6 +85,11 @@ public class RobotMap {
 		public static PDP pdp;
 		public static PositionEncodedMotor leftWheel;
 		public static PositionEncodedMotor rightWheel;
+		public static CANEncoder leftWheelEncoder;
+		public static CANEncoder rightWheelEncoder;
+		public static Flywheel flywheel;
+		public static Dump ballDumper;
+		public static BallInnie ballIntake;
 		public static Subsystem[] mainSubsystems;
 	}
 	
@@ -95,6 +106,22 @@ public class RobotMap {
 	
 	public RobotMap() {
 		Component.pdp = new PDP();
+		// Chassis
+		Component.leftWheelEncoder = new CANEncoder(Port.CAN.leftEncoder);
+		Component.leftWheelEncoder.setReverseDirection(true);
+		Component.leftWheel = new PositionEncodedMotor("leftWheel", new AccelerationCap(Component.pdp), new CustomPIDController(Component.leftWheelEncoder), new VictorSP(Port.PWM.leftDriveMotor));
+		Component.leftWheel.disablePID(); // TODO add encoders
+		Component.leftWheel.setInverted(true);
+		Component.rightWheelEncoder = new CANEncoder(Port.CAN.rightEncoder);
+		Component.rightWheel = new PositionEncodedMotor("rightWheel", new AccelerationCap(Component.pdp), new CustomPIDController(Component.rightWheelEncoder), new VictorSP(Port.PWM.rightDriveMotor));
+		Component.rightWheel.disablePID(); // TODO add encoders
+		Component.rightWheel.setInverted(false);
+		// Ball Intake
+		Component.ballIntake = new BallInnie(new VictorSP(Port.PWM.ballInnie));
+		// Ball Dumper
+		Component.ballDumper = new Dump(new VictorSP(Port.PWM.vomitElevator), new VictorSP(Port.PWM.vomitOuttakeRoller));
+		// Flywheel
+		Component.flywheel = new Flywheel(new VictorSP(Port.PWM.flywheelLeftMotor), new VictorSP(Port.PWM.flywheelRightMotor));
 		// Human inputs
 		HumanInput.Operator.stick = new CustomJoystick(Port.HumanInput.joystick);
 		HumanInput.Operator.stick.setDeadzone(Constant.HumanInput.OPERATOR_JOYSTICK_MINIMUM_THRESHOLD);
