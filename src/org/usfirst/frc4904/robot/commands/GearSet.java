6f2216eff5796ca1677bbaffe133d47c709e9466
Nikhil Suresh <Nikhil Suresh>
2017-01-16 14:41:38 -0800

    Got rid of excess methods

--- src/org/usfirst/frc4904/robot/commands/GearSet.java
+++ src/org/usfirst/frc4904/robot/commands/GearSet.java
@@ -17,12 +17,6 @@ public class GearSet extends Command {
 		this.state = state;
 	}
 	
-	// Called just before this Command runs the first time
-	@Override
-	protected void initialize() {
-		
-	}
-	
 	// Called repeatedly when this Command is scheduled to run
 	@Override
 	protected void execute() {
@@ -34,13 +28,4 @@ public class GearSet extends Command {
 	protected boolean isFinished() {
 		return false;
 	}
-	
-	// Called once after isFinished returns true
-	@Override
-	protected void end() {}
-	
-	// Called when another command which requires one or more of the same
-	// subsystems is scheduled to run
-	@Override
-	protected void interrupted() {}
 }
