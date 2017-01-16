8943a6e5f2ea7a06f4e124480f56ff239cd73b69
Billy <wwpierce020602@gmail.com>
2017-01-16 14:24:15 -0800

    Switched Outtake to CommandGroup

--- src/org/usfirst/frc4904/robot/commands/StartOuttakeMotors.java
+++ src/org/usfirst/frc4904/robot/commands/StartOuttakeMotors.java
@@ -2,50 +2,18 @@ package org.usfirst.frc4904.robot.commands;
 
 
 import org.usfirst.frc4904.robot.RobotMap;
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
  *
  */
-public class StartOuttakeMotors extends Command {
+public class StartOuttakeMotors extends CommandGroup {
 	
 	public StartOuttakeMotors() {
 		// Use requires() here to declare subsystem dependencies
 		requires(RobotMap.Component.ballIO);
-	}
-	
-	// Called just before this Command runs the first time
-	@Override
-	protected void initialize() {
-		RobotMap.Component.ballIO.topMotor.set(-1.0);
-		RobotMap.Component.ballIO.leftMotor.set(0);
-		RobotMap.Component.ballIO.mainMotor.set(1.0);
-	}
-	
-	// Called repeatedly when this Command is scheduled to run
-	@Override
-	protected void execute() {}
-	
-	// Make this return true when this Command no longer needs to run execute()
-	@Override
-	protected boolean isFinished() {
-		return false;
-	}
-	
-	// Called once after isFinished returns true
-	@Override
-	protected void end() {
-		RobotMap.Component.ballIO.topMotor.set(0);
-		RobotMap.Component.ballIO.leftMotor.set(0);
-		RobotMap.Component.ballIO.mainMotor.set(0);
-	}
-	
-	// Called when another command which requires one or more of the same
-	// subsystems is scheduled to run
-	@Override
-	protected void interrupted() {
-		RobotMap.Component.ballIO.topMotor.set(0);
-		RobotMap.Component.ballIO.leftMotor.set(0);
-		RobotMap.Component.ballIO.mainMotor.set(0);
+		addParallel(MotorConstant(RobotMap.Component.ballIO.topMotor, -1.0));
+		addParallel(MotorConstant(RobotMap.Component.ballIO.leftMotor, 0));
+		addParallel(MotorConstant(RobotMap.Component.ballIO.mainMotor, 1.0));
 	}
 }
