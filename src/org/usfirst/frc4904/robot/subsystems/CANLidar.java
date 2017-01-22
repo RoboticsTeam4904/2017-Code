a7f5b6dcc622d6addfa44b419f2978144d814547
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2017-01-22 09:47:23 -0800

    added some helpful commentsto canlidar

--- src/org/usfirst/frc4904/robot/subsystems/CANLidar.java
+++ src/org/usfirst/frc4904/robot/subsystems/CANLidar.java
@@ -16,7 +16,7 @@ public class CANLidar extends CustomCAN {
 	 * @param modes
 	 *        Number of modes for the CAN sensor
 	 */
-	public CANLidar(String name, int id) {
+	public CANLidar(String name, int id) {// get the name and id of lidar
 		super(name, id);
 	}
 	
@@ -35,14 +35,16 @@ public class CANLidar extends CustomCAN {
 	 * 
 	 */
 	public int[] readlidar() throws InvalidSensorException {
-		write(new byte[] {0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01});// write to trigger read
+		write(new byte[] {0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01}); // Write to trigger read
 		ByteBuffer rawData = readBuffer();
-		if (rawData != null && rawData.remaining() > 7) {
-			rawData.rewind();
+		if (rawData != null && rawData.remaining() > 7) { // makes sure each byte is 8 ints long
+			rawData.rewind(); // puts them all together
 			long data = Long.reverseBytes(rawData.getLong());
-			int xCoordinate = (int) (data & 0xFFFFFFFF);
-			int yCoordinate = (int) (data >> 32);
-			return new int[] {xCoordinate, yCoordinate};
+			int xCoordinate = (int) (data & 0xFFFFFFFF); // first 32 ints are x coordinate
+			int yCoordinate = (int) (data >> 32); // makes second 32 ints y coordinate
+			if (yCoordinate == 1) { // if coordinate is valid, return the coordinates
+				return new int[] {xCoordinate, yCoordinate};
+			}
 		}
 		return null;
 	}
