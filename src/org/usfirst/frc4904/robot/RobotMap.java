4a45fbebfae4ffca6fbd29ddba47db2cf8632ad5
Billy <wwpierce020602@gmail.com>
2017-01-23 18:15:37 -0800

    Added Driving code to robotmap.java

--- src/org/usfirst/frc4904/robot/RobotMap.java
+++ src/org/usfirst/frc4904/robot/RobotMap.java
@@ -68,55 +68,89 @@ public class RobotMap {
 		
 		public static class PCM {}
 		
-		public static class Pneumatics {}
+		public static class Pneumatics {
+			public static int solenoidUp = 0;
+			public static int solenoidDown = 1;
+		}
+	}
+	
+	public static class Constant {
+		public static class HumanInput {
+			public static final double Y_SPEED_SCALE = 1;
+			public static final double TURN_SPEED_SCALE = 1;
+			public static final double XBOX_MINIMUM_THRESHOLD = 0.1;
+			public static final double SPEED_GAIN = 1;
+			public static final double SPEED_EXP = 2;
+			public static final double TURN_GAIN = 1;
+			public static final double TURN_EXP = 2;
+		}
+		
+		public static class Chassis {
+			public static double TURN_P = 0.02;
+			public static double TURN_I = 0.001;
+			public static double TURN_D = 0.3;
+			public static final double MAX_DEGREES_PER_SECOND = 120;
+			public static final double WHEEL_RADIUS_INCHES = 8;
+			public static final double WHEEL_PULSES_PER_REVOLUTION = 1024;
+		}
+	}
+	
+	public static class HumanInput {
+		public static class Driver {
+			public static CustomXbox xbox;
+		}
+		
+		public static class Operator {
+			public static CustomJoystick stick;
+		}
 	}
 	
 	public static class Component {
 		public static PDP pdp;
-		public static Chassis chassis;
-		public static PositionEncodedMotor leftWheel;
-		public static PositionEncodedMotor rightWheel;
+		public static TankDriveShifting chassis;
+		public static Motor leftWheel;
+		public static Motor rightWheel;
 		public static CustomEncoder leftWheelEncoder;
 		public static CustomEncoder rightWheelEncoder;
+		public static MotionController chassisEncoderMC;
 		public static Flywheel flywheel;
 		public static Dump ballDumper;
 		public static BallInnie ballIntake;
 		public static Subsystem[] mainSubsystems;
 		public static CustomXbox driverXbox;
 		public static CustomJoystick operatorStick;
-		// Vision
 		public static AligningCamera alignCamera;
+		public static CustomPIDController chassisMC;
+		public static NavX navx;
+		public static SolenoidShifters shifter;
 	}
 	
 	public RobotMap() {
 		Component.pdp = new PDP();
-		// Chassis
+		Component.shifter = new SolenoidShifters(Port.Pneumatics.solenoidUp, Port.Pneumatics.solenoidDown);
+		Component.navx = new NavX(SerialPort.Port.kMXP);
+		Component.chassisMC = new CustomPIDController(0.01, 0.0, -0.02, RobotMap.Component.navx);
+		Component.chassisMC.setInputRange(-180, 180);
+		Component.chassisMC.setContinuous(true);
+		Component.leftWheelEncoder = new CANEncoder("LeftEncoder", Port.CAN.leftEncoder, false);
+		Component.rightWheelEncoder = new CANEncoder("RightEncoder", Port.CAN.rightEncoder, false);
+		Component.chassisEncoderMC = new CustomPIDController(0.001, 0.0, -0.002, new EncoderGroup(100, Component.leftWheelEncoder, Component.rightWheelEncoder));
+		Component.leftWheel = new Motor("LeftWheel", false, new AccelerationCap(Component.pdp), new CANTalon(Port.Motors.CAN.leftDriveA), new CANTalon(Port.Motors.CAN.leftDriveB));
+		Component.rightWheel = new Motor("RightWheel", false, new AccelerationCap(Component.pdp), new CANTalon(Port.Motors.CAN.rightDriveA), new CANTalon(Port.Motors.CAN.rightDriveB));
 		Component.leftWheelEncoder = new CANEncoder(Port.CAN.leftEncoder);
-		Component.leftWheelEncoder.setReverseDirection(true);
-		Component.leftWheel = new PositionEncodedMotor("leftWheel", new AccelerationCap(Component.pdp), new CustomPIDController(Component.leftWheelEncoder), new VictorSP(Port.PWM.leftDriveMotor));
-		Component.leftWheel.disablePID(); // TODO add encoders
-		// Component.leftWheel.setInverted(true);
 		Component.rightWheelEncoder = new CANEncoder(Port.CAN.rightEncoder);
-		Component.rightWheel = new PositionEncodedMotor("rightWheel", new AccelerationCap(Component.pdp), new CustomPIDController(Component.rightWheelEncoder), new VictorSP(Port.PWM.rightDriveMotor));
-		Component.rightWheel.disablePID(); // TODO add encoders
-		// Component.rightWheel.setInverted(false);
-		// Ball Intake
-		Component.ballIntake = new BallInnie(new VictorSP(Port.PWM.ballInnie));
-		// Ball Dumper
-		Component.ballDumper = new Dump(new VictorSP(Port.PWM.vomitElevator), new VictorSP(Port.PWM.vomitOuttakeRoller));
-		// Flywheel
-		Motor leftFlywheelMotor = new Motor(new VictorSP(Port.PWM.flywheelLeftMotor));
-		leftFlywheelMotor.setInverted(true);
-		Motor rightFlywheelMotor = new Motor(new VictorSP(Port.PWM.flywheelRightMotor));
-		rightFlywheelMotor.setInverted(false);
-		Component.flywheel = new Flywheel(leftFlywheelMotor, rightFlywheelMotor);
+		double inchesPerPulse = 2 * Math.PI * Constant.Chassis.WHEEL_RADIUS_INCHES / Constant.Chassis.WHEEL_PULSES_PER_REVOLUTION;
+		Component.leftWheelEncoder.setDistancePerPulse(inchesPerPulse);
+		Component.rightWheelEncoder.setDistancePerPulse(inchesPerPulse);
+		Component.chassis = new TankDriveShifting("2017-Chassis", Component.leftWheel, Component.rightWheel, Component.shifter);
 		// Human inputs
 		Component.operatorStick = new CustomJoystick(Port.HumanInput.joystick);
 		Component.operatorStick.setDeadzone(DefaultOperator.JOYSTICK_MIN_THRESH);
-		Component.driverXbox = new CustomXbox(Port.HumanInput.xboxController);
-		Component.driverXbox.setDeadZone(DefaultDriver.CONTROLLER_MIN_THRESH);
+		HumanInput.Driver.xbox = new CustomXbox(Port.HumanInput.xboxController);
+		HumanInput.Operator.stick = new CustomJoystick(Port.HumanInput.joystick);
+		HumanInput.Driver.xbox.setDeadZone(Constant.HumanInput.XBOX_MINIMUM_THRESHOLD);
 		// Main Subsystems
 		Component.alignCamera = new AligningCamera(PIDSourceType.kRate);
-		Component.mainSubsystems = new Subsystem[] {Component.ballIntake, Component.ballDumper, Component.flywheel};
+		Component.mainSubsystems = new Subsystem[] {};// Component.ballIntake, Component.ballDumper, Component.flywheel};
 	}
 }
