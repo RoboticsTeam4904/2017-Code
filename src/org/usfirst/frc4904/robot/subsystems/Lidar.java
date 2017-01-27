f0adb9fefdd00c57f9cee03356b6c4e78b83f74a
Ariana Lissak <ariliss@nuevaschool.org>
2017-01-27 11:55:28 -0800

    set constants differently

--- src/org/usfirst/frc4904/robot/subsystems/Lidar.java
+++ src/org/usfirst/frc4904/robot/subsystems/Lidar.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc4904.robot.subsystems;
 
 
+import org.usfirst.frc4904.standard.commands.motor.MotorConstant;
 import org.usfirst.frc4904.standard.custom.motioncontrollers.CustomPIDController;
 import org.usfirst.frc4904.standard.custom.sensors.CustomEncoder;
 import org.usfirst.frc4904.standard.subsystems.motor.VelocityEncodedMotor;
@@ -10,11 +11,16 @@ public class Lidar extends VelocityEncodedMotor {
 	public static final double LIDAR_TURN_P = 0;// WIP
 	public static final double LIDAR_TURN_I = 0;// WIP
 	public static final double LIDAR_TURN_D = 0;// WIP
-	public static final double LIDAR_P_FORWARDS = 0.3;
+	public static final double LIDAR_P_FORWARDS = 0.00675;
 	public static final double LIDAR_DIAMETER = 3.107;// inches;
 	public static final double LIDAR_CIRCUMFERENCE = Lidar.LIDAR_DIAMETER * Math.PI;
 	public static final double LIDAR_RPS = 4;
 	public static final double LIDAR_ENCODER_PPR = 10;// WIP-- carter said I could choose a value
+	
+	@Override
+	protected void initDefaultCommand() {
+		setDefaultCommand(new MotorConstant(this, 0.275));
+	}
 	
 	public Lidar(CustomEncoder encoder, SpeedController... motors) {// must add lidar data later
 		super("LIDAR", false, new CustomPIDController(Lidar.LIDAR_TURN_P, Lidar.LIDAR_TURN_I, Lidar.LIDAR_TURN_D, Lidar.LIDAR_P_FORWARDS, encoder), motors);
