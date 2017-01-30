bdb66c670ee263e897492923380a68b05a4e0af7
Yoni Lerner <yonilern@gmail.com>
2017-01-30 12:49:39 -0800

    Removed RobotMap constants

--- src/org/usfirst/frc4904/robot/RobotMap.java
+++ src/org/usfirst/frc4904/robot/RobotMap.java
@@ -80,20 +80,6 @@ public class RobotMap {
 		}
 	}
 
-	public static class Constant {
-		public static class HumanInput {
-			public static final double X_SPEED_SCALE = 1;
-			public static final double Y_SPEED_SCALE = 1;
-			public static final double SPEED_GAIN = 1;
-			public static final double SPEED_EXP = 2;
-			public static final double TURN_GAIN = 1;
-			public static final double TURN_EXP = 2;
-			public static final double TURN_SPEED_SCALE = 1;
-			public static final double XBOX_MINIMUM_THRESHOLD = 0.1;
-			public static final double OPERATOR_JOYSTICK_MINIMUM_THRESHOLD = 0.1;
-		}
-	}
-
 	public static class PCM {}
 
 	public static class Pneumatics {
