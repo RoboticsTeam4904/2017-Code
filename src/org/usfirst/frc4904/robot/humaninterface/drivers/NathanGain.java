c97fa6b73ea51132a8b23e733e219eb3e185e919
Howard Stark <howard@getcoffee.io>
2017-01-30 12:05:15 -0800

    Added trimming to NathanGain for GearAlign

--- src/org/usfirst/frc4904/robot/humaninterface/drivers/NathanGain.java
+++ src/org/usfirst/frc4904/robot/humaninterface/drivers/NathanGain.java
@@ -2,6 +2,10 @@ package org.usfirst.frc4904.robot.humaninterface.drivers;
 
 
 import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.robot.vision.AligningCameraGRIP;
+import org.usfirst.frc4904.sovereignty.TrimCommand;
+import org.usfirst.frc4904.sovereignty.TrimCommand.TrimDirection;
+import org.usfirst.frc4904.sovereignty.strategies.GearAlign;
 import org.usfirst.frc4904.standard.commands.chassis.ChassisShift;
 import org.usfirst.frc4904.standard.humaninput.Driver;
 import org.usfirst.frc4904.standard.subsystems.chassis.SolenoidShifters;
@@ -13,6 +17,7 @@ public class NathanGain extends Driver {
 	public static final double TURN_EXP = 2;
 	public static final double Y_SPEED_SCALE = 1;
 	public static final double TURN_SPEED_SCALE = 1;
+	private final GearAlign gearAlign = new GearAlign(new AligningCameraGRIP());
 
 	public NathanGain() {
 		super("NathanGain");
@@ -28,6 +33,10 @@ public class NathanGain extends Driver {
 			.whenPressed(new ChassisShift(RobotMap.Component.chassis.getShifter(), SolenoidShifters.ShiftState.DOWN));
 		RobotMap.Component.xbox.b
 			.whenPressed(new ChassisShift(RobotMap.Component.chassis.getShifter(), SolenoidShifters.ShiftState.UP));
+		RobotMap.Component.xbox.dPad.left
+			.whenPressed(new TrimCommand(gearAlign, TrimDirection.LEFT));
+		RobotMap.Component.xbox.dPad.right
+			.whenPressed(new TrimCommand(gearAlign, TrimDirection.RIGHT));
 	}
 
 	@Override
