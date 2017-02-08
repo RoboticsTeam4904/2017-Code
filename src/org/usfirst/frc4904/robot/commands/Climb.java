fa2cd9dab07d23bab50a63625dc4842285cf0531
Yoni Lerner <yonilern@gmail.com>
2017-02-08 10:33:55 -0800

    Refactored StartClimber

--- src/org/usfirst/frc4904/robot/commands/Climb.java
+++ src/org/usfirst/frc4904/robot/commands/Climb.java
@@ -5,8 +5,8 @@ import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.subsystems.Climber;
 import org.usfirst.frc4904.standard.commands.motor.MotorConstant;
 
-public class StartClimber extends MotorConstant {
-	public StartClimber() {
+public class Climb extends MotorConstant {
+	public Climb() {
 		super(RobotMap.Component.climber, Climber.CLIMBER_TARGET_SPEED);
 	}
 }
