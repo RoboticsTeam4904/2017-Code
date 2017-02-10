03266faadc0e1c4f934bfe96b859d94603c63d15
Yoni Lerner <yonilern@gmail.com>
2017-02-10 15:11:22 -0800

    Added yet another safety precaution for backspinning the climber

--- src/org/usfirst/frc4904/robot/subsystems/Climber.java
+++ src/org/usfirst/frc4904/robot/subsystems/Climber.java
@@ -2,10 +2,11 @@ package org.usfirst.frc4904.robot.subsystems;
 
 
 import org.usfirst.frc4904.standard.subsystems.motor.Motor;
+import org.usfirst.frc4904.standard.subsystems.motor.speedmodifiers.CapSpeedModifier;
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class Climber extends Motor {
 	public Climber(SpeedController climbMotor) {
-		super(climbMotor);
+		super(new CapSpeedModifier(0, 1), climbMotor); // Really REALLY don't give this motor any nonpositive values
 	}
 }
