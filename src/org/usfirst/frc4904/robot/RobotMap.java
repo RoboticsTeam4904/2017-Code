7c2340453d39c8326913da1c4d6dde817c6b8ae3
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2017-02-10 16:51:16 -0800

    made antiJam only run if the button that disables it is not pressed

--- src/org/usfirst/frc4904/robot/RobotMap.java
+++ src/org/usfirst/frc4904/robot/RobotMap.java
@@ -39,6 +39,7 @@ public class RobotMap {
 		public static class HumanInput {
 			public static final int joystick = 0;
 			public static final int xboxController = 1;
+			public static final int teensyStick = 2;
 		}
 
 		public static class Motors {
@@ -90,6 +91,7 @@ public class RobotMap {
 	public static class Component {
 		public static CustomXbox driverXbox;
 		public static CustomJoystick operatorStick;
+		public static CustomJoystick teensyStick;
 		public static PDP pdp;
 		public static SolenoidShifters shifter;
 		public static TankDriveShifting chassis;
@@ -141,6 +143,7 @@ public class RobotMap {
 		// Human inputs
 		Component.operatorStick = new CustomJoystick(Port.HumanInput.joystick);
 		Component.operatorStick.setDeadzone(DefaultOperator.JOYSTICK_MIN_THRESH);
+		Component.teensyStick = new CustomJoystick(Port.HumanInput.teensyStick);
 		Component.driverXbox = new CustomXbox(Port.HumanInput.xboxController);
 		Component.driverXbox.setDeadZone(DefaultDriver.XBOX_MINIMUM_THRESHOLD);
 		// Main Subsystems
--- src/org/usfirst/frc4904/robot/commands/ShooterStart.java
+++ src/org/usfirst/frc4904/robot/commands/ShooterStart.java
@@ -3,15 +3,20 @@ package org.usfirst.frc4904.robot.commands;
 
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.standard.commands.RunFor;
+import org.usfirst.frc4904.standard.commands.RunIf;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class ShooterStart extends CommandGroup {
 	public final static double unloadDuration = 0.25; // TODO
 
+	boolean anything() {
+		return true;
+	}
+
 	public ShooterStart() {
 		addParallel(new FlywheelSpinup());
-		addParallel(new RunFor(new IndexerUnload(), ShooterStart.unloadDuration));
+		addParallel(new RunIf(new RunFor(new IndexerUnload(), ShooterStart.unloadDuration), this::antiJamOn));
 		addSequential(new Command() {
 			@Override
 			protected boolean isFinished() {
@@ -19,5 +24,9 @@ public class ShooterStart extends CommandGroup {
 			}
 		});
 		addParallel(new IndexerLoad());
+	}
+
+	private boolean antiJamOn() {
+		return !RobotMap.Component.teensyStick.button5.get();
 	}
 }
