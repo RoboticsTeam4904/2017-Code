42b4772bb34075726e388b12967b9be7bd701890
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2017-02-10 16:19:16 -0800

    added anti jam

--- src/org/usfirst/frc4904/robot/commands/ShooterStart.java
+++ src/org/usfirst/frc4904/robot/commands/ShooterStart.java
@@ -2,12 +2,16 @@ package org.usfirst.frc4904.robot.commands;
 
 
 import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.standard.commands.RunFor;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class ShooterStart extends CommandGroup {
+	public final static double unloadDuration = 0.25; // TODO
+
 	public ShooterStart() {
 		addParallel(new FlywheelSpinup());
+		addParallel(new RunFor(new IndexerUnload(), ShooterStart.unloadDuration));
 		addSequential(new Command() {
 			@Override
 			protected boolean isFinished() {
