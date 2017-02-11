d2f52452c8a06a5690c58306da906381000dcaf3
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2017-02-10 18:46:11 -0800

    made overridable command tomake it easier for commands to override other commands

---
+++ src/org/usfirst/frc4904/robot/OverridableCommand.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc4904.robot;
+
+
+/**
+ * 
+ * @author niksure
+ *         This interface allows commands to easily override other commands
+ */
+public interface OverridableCommand {
+	/**
+	 * sets an internal override to an boolean you give it
+	 * 
+	 * @param override
+	 */
+	void setOverride(boolean override);
+
+	/**
+	 * returns if override is on or off
+	 * 
+	 * @return
+	 */
+	boolean getOverride();
+}
