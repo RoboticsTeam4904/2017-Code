603e15e7ce622a3aa5a09cfa216aad865ad0dc7b
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2017-02-12 12:59:57 -0800

    made PR changes

--- src/org/usfirst/frc4904/robot/commands/AutoShifter.java
+++ src/org/usfirst/frc4904/robot/commands/AutoShifter.java
@@ -4,6 +4,7 @@ package org.usfirst.frc4904.robot.commands;
 import java.util.LinkedList;
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.standard.custom.sensors.CustomEncoder;
+import org.usfirst.frc4904.standard.custom.sensors.NavX;
 import org.usfirst.frc4904.standard.subsystems.chassis.AutoSolenoidShifters;
 import org.usfirst.frc4904.standard.subsystems.chassis.SolenoidShifters;
 import edu.wpi.first.wpilibj.command.Command;
@@ -12,9 +13,10 @@ public class AutoShifter extends Command {
 	protected final CustomEncoder leftEncoder;
 	protected final CustomEncoder rightEncoder;
 	protected final AutoSolenoidShifters shifter;
+	protected final NavX navX;
 	protected final LinkedList<Double> speeds = new LinkedList<Double>();
-	public static final double RAPID_ACCELERATION = 8;// WIP
-	public static final double RAPID_DECELERATION = 2;// WIP
+	public static final double RAPID_ACCELERATION = 0.24881632653;// in Gs--TODO
+	public static final double RAPID_DECELERATION = -0.06216;// in Gs--TODO
 	public static final double RATE_DIFF = 100; // encoder ticks--WIP
 	public static final double SUPER_SLOW_RATE = 2; // ft per sec
 	public static final double MEDIUM_RATE = AutoShifter.SUPER_SLOW_RATE * 2;
@@ -26,26 +28,26 @@ public class AutoShifter extends Command {
 	public static final double LAST_MANUAL_SHIFT_TIME = 5000; // TODO
 	public static final double LAST_AUTO_SHIFT_TIME = 500;// TODO
 
-	public AutoShifter(AutoSolenoidShifters shifter, CustomEncoder leftEncoder, CustomEncoder rightEncoder) {
+	public AutoShifter(AutoSolenoidShifters shifter, CustomEncoder leftEncoder, CustomEncoder rightEncoder, NavX navX) {
 		this.shifter = shifter;
 		this.leftEncoder = leftEncoder;
 		this.rightEncoder = rightEncoder;
+		this.navX = navX;
 	}
 
 	public AutoShifter() {
-		shifter = RobotMap.Component.shifter;
-		leftEncoder = RobotMap.Component.leftWheelEncoder;
-		rightEncoder = RobotMap.Component.rightWheelEncoder;
+		this(RobotMap.Component.shifter, RobotMap.Component.leftWheelEncoder, RobotMap.Component.rightWheelEncoder,
+			RobotMap.Component.navx);
 	}
 
 	@Override
 	protected void execute() {
 		double speed = (leftEncoder.getRate() + rightEncoder.getRate()) / 2;
+		double acceleration = navX.getWorldLinearAccelY();
 		speeds.add(speed);
 		while (speeds.size() > AutoShifter.SPEED_LIST_MAX_LENGTH) {
 			speeds.removeFirst();
 		}
-		double speedDiffAvg = (speeds.getLast() - speeds.getFirst()) / (speeds.size() - 1);// acceleration
 		if (leftEncoder.getRate() - rightEncoder.getRate() < AutoShifter.RATE_DIFF
 			&& shifter.timeSinceLastManualShift() > AutoShifter.LAST_MANUAL_SHIFT_TIME
 			&& shifter.timeSinceLastAutoShift() > AutoShifter.LAST_AUTO_SHIFT_TIME) {// only if driving straight and we haven't manually shifted in 5 seconds or autoshifted in 0.5 seconds
@@ -54,10 +56,10 @@ public class AutoShifter extends Command {
 				throttle += motorSpeed;
 			}
 			throttle /= RobotMap.Component.chassis.getMotorSpeeds().length;
-			if (Math.abs(speed) > AutoShifter.MEDIUM_RATE && Math.abs(speedDiffAvg) > AutoShifter.RAPID_ACCELERATION
-				&& throttle > AutoShifter.FAST_THROTTLE) { // 4 ft/s
+			if (Math.abs(speed) > AutoShifter.MEDIUM_RATE && (acceleration) > AutoShifter.RAPID_ACCELERATION
+				&& throttle > AutoShifter.FAST_THROTTLE) {
 				shifter.shift(SolenoidShifters.ShiftState.UP, true);
-			} else if ((Math.abs(speed) < AutoShifter.FAST_RATE && Math.abs(speedDiffAvg) < AutoShifter.RAPID_DECELERATION
+			} else if ((Math.abs(speed) < AutoShifter.FAST_RATE && (acceleration) < AutoShifter.RAPID_DECELERATION
 				&& throttle > AutoShifter.MEDIUM_THROTTLE)
 				|| (Math.abs(speed) < AutoShifter.SUPER_SLOW_RATE && throttle < AutoShifter.SLOW_THROTTLE)) {
 				shifter.shift(SolenoidShifters.ShiftState.DOWN, true);
--- src/org/usfirst/frc4904/robot/humaninterface/HumanInterfaceConfig.java
+++ src/org/usfirst/frc4904/robot/humaninterface/HumanInterfaceConfig.java
@@ -1,15 +1,13 @@
 package org.usfirst.frc4904.robot.humaninterface;
 
 
-import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.commands.AutoShifter;
 import org.usfirst.frc4904.robot.vision.AligningCameraGRIP;
 import org.usfirst.frc4904.sovereignty.strategies.GearAlign;
 
 public class HumanInterfaceConfig {
 	public static final GearAlign gearAlign = new GearAlign(new AligningCameraGRIP());
-	public static final AutoShifter autoShifter = new AutoShifter(RobotMap.Component.shifter,
-		RobotMap.Component.leftWheelEncoder, RobotMap.Component.rightWheelEncoder);
+	public static final AutoShifter autoShifter = new AutoShifter();
 
 	private HumanInterfaceConfig() {}
 }
