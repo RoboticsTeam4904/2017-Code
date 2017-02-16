03315438f56306d2528bf17a1929832d7e3db6ed
CaseyManning <kcgrind@gmail.com>
2017-02-15 16:38:12 -0800

    fixed hopper agitation so that it actually works

--- src/org/usfirst/frc4904/robot/commands/HopperAgitate.java
+++ src/org/usfirst/frc4904/robot/commands/HopperAgitate.java
@@ -1,31 +1,26 @@
 package org.usfirst.frc4904.robot.commands;
 
-
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.subsystems.Hopper;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 public class HopperAgitate extends Command {
-	protected long StartTime;
+	protected long lastSwitchTime;
 
 	public HopperAgitate() {
 		requires(RobotMap.Component.hopper);
 	}
 
 	@Override
-	protected void initialize() {
-		StartTime = System.currentTimeMillis();
-		RobotMap.Component.hopper.setState(Hopper.HopperState.BALLIO);
-	}
-
-	@Override
 	protected void execute() {
-		if (System.currentTimeMillis() - StartTime > Hopper.AGITATE_DELAY) {
+		if (System.currentTimeMillis() - lastSwitchTime > Hopper.AGITATE_DELAY) {
 			if (RobotMap.Component.hopper.getState() == Hopper.HopperState.BALLIO) {
 				RobotMap.Component.hopper.setState(Hopper.HopperState.SHOOTER);
 			} else {
 				RobotMap.Component.hopper.setState(Hopper.HopperState.BALLIO);
 			}
+			lastSwitchTime = System.currentTimeMillis();
 		}
 	}
 
--- src/org/usfirst/frc4904/robot/subsystems/Hopper.java
+++ src/org/usfirst/frc4904/robot/subsystems/Hopper.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc4904.robot.subsystems;
 
-
 import org.usfirst.frc4904.standard.commands.Idle;
+
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -21,6 +21,7 @@ public class Hopper extends Subsystem {
 			return value;
 		}
 	}
+
 	protected HopperState currentState;
 
 	public Hopper(DoubleSolenoid solenoid) {
