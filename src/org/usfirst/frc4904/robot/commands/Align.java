09eff83bad940eb86fb2b236c93d5091558a1aea
Ariana Lissak <ariliss@nuevaschool.org>
2017-02-15 17:15:29 -0800

    ShootCycle Class
    class that handles align pinup and shoot with button 4

---
+++ src/org/usfirst/frc4904/robot/commands/Align.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc4904.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Align extends Command {
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
