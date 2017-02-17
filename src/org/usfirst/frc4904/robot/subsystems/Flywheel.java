08abc9976ade4a475bb5bf8b4a81d9ae91e055db
Yoni Lerner <yonilern@gmail.com>
2017-02-17 09:06:58 -0800

    Adjusted shooting speed

--- src/org/usfirst/frc4904/robot/subsystems/Flywheel.java
+++ src/org/usfirst/frc4904/robot/subsystems/Flywheel.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj.SpeedController;
 
 public class Flywheel extends VelocitySensorMotor {
 	// TODO: Tune this
-	public static final double SHOOTING_SPEED = 0.75;
+	public static final double SHOOTING_SPEED = 0.55;
 	public static final double FLYWHEEL_P = 0.0;
 	public static final double FLYWHEEL_I = 0.0;
 	public static final double FLYWHEEL_D = 0.0;
