6bcdf823f0223b79d996f26f9f7f7130dd369b6d
Yoni Lerner <yonilern@gmail.com>
2017-02-17 20:50:17 -0800

    Make sure the ramp is extended every time we intake

--- src/org/usfirst/frc4904/robot/commands/GearioIntake.java
+++ src/org/usfirst/frc4904/robot/commands/GearioIntake.java
@@ -1,10 +1,18 @@
 package org.usfirst.frc4904.robot.commands;
 
 
+import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.subsystems.GearIO;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class GearioIntake extends GearSet {
 	public GearioIntake() {
 		super(GearIO.GearState.INTAKE);
+	}
+
+	@Override
+	protected void initialize() {
+		super.initialize();
+		RobotMap.Component.gearIO.setRampState(DoubleSolenoid.Value.kForward);
 	}
 }
