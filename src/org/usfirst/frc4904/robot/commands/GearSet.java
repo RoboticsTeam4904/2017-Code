bf960ad1214f6dff3c0f0845239f5da25f864ae7
Yoni Lerner <yonilern@gmail.com>
2017-02-17 20:47:55 -0800

    Started referencing GearState explicitly

--- src/org/usfirst/frc4904/robot/commands/GearSet.java
+++ src/org/usfirst/frc4904/robot/commands/GearSet.java
@@ -3,11 +3,10 @@ package org.usfirst.frc4904.robot.commands;
 
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.subsystems.GearIO;
-import org.usfirst.frc4904.robot.subsystems.GearIO.GearState;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class GearSet extends Command {
-	protected final GearState state;
+	protected final GearIO.GearState state;
 
 	public GearSet(GearIO.GearState state) {
 		this.state = state;
