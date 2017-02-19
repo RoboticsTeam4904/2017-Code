d3037704610e8c1291f0697adb4db54f88c6e814
AJ Nadel <aj@ajnadel.com>
2017-02-19 11:56:22 -0800

    improve BallioDoorSet commands for safety

--- src/org/usfirst/frc4904/robot/commands/BallioDoorSetIntake.java
+++ src/org/usfirst/frc4904/robot/commands/BallioDoorSetIntake.java
@@ -11,12 +11,19 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class BallioDoorSetIntake extends CommandGroup {
+	private static final double HOPPER_ROLLERS_CLEAR_SPEED = -0.5;
+	private static final double ELEVATOR_CLEAR_SPEED = 0.5;
+	private static final double SERVO_PATH_CLEAR_TIME = 0.25;
+
 	public BallioDoorSetIntake() {
-		Command hopperRollersDown = new MotorConstant(RobotMap.Component.ballIO.hopperRollers, -0.5);
-		Command intakeRollersUp = new MotorConstant(RobotMap.Component.ballIO.elevatorAndIntakeRoller, 0.5);
-		Command clearServoPath = new RunFor(new RunAllParallel(hopperRollersDown, intakeRollersUp), 0.25);
+		Command hopperRollersDown = new MotorConstant(RobotMap.Component.ballIO.hopperRollers,
+			BallioDoorSetIntake.HOPPER_ROLLERS_CLEAR_SPEED);
+		Command elevatorsUp = new MotorConstant(RobotMap.Component.ballIO.elevatorAndIntakeRoller,
+			BallioDoorSetIntake.ELEVATOR_CLEAR_SPEED);
+		Command clearServoPath = new RunFor(new RunAllParallel(hopperRollersDown, elevatorsUp),
+			BallioDoorSetIntake.SERVO_PATH_CLEAR_TIME);
 		addSequential(new RunUnless(clearServoPath, this::isAlreadySetToIntake));
-		addSequential(new BallioDoorSet(BallIO.DoorState.INTAKE));
+		addSequential(new BallioDoorSetUnsafely(BallIO.DoorState.INTAKE));
 	}
 
 	boolean isAlreadySetToIntake() {
