a0e7f2a8a70a900fdbf090cd5dc9fdfae558095a
Ariana Lissak <ariliss@nuevaschool.org>
2017-02-19 15:42:13 -0800

    wrote auto code for dead reckoning place gear

--- src/org/usfirst/frc4904/robot/autonomous/strategies/AutonPlaceGear.java
+++ src/org/usfirst/frc4904/robot/autonomous/strategies/AutonPlaceGear.java
@@ -1,5 +1,16 @@
 package org.usfirst.frc4904.robot.autonomous.strategies;
 
-public class AutonPlaceGear {
+import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.robot.subsystems.GearIO;
+import org.usfirst.frc4904.standard.commands.chassis.ChassisMoveDistance;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class AutonPlaceGear extends CommandGroup {
+	public AutonPlaceGear() {
+		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis, 38.5, RobotMap.Component.chassisDriveMC,
+				RobotMap.Component.leftWheelEncoder, RobotMap.Component.rightWheelEncoder));
+		RobotMap.Component.gearIO.setState(GearIO.GearState.OUTTAKE);
+	}
 
 }
