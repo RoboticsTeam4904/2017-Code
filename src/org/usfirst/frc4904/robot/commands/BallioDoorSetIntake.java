7bb1ea2fdf0998e50c9eac6a528424a829b1b8ab
AJ Nadel <aj@ajnadel.com>
2017-02-18 16:16:50 -0800

    add safety conditions for HopperSetShooter and ballio intake

--- src/org/usfirst/frc4904/robot/commands/BallioDoorSetIntake.java
+++ src/org/usfirst/frc4904/robot/commands/BallioDoorSetIntake.java
@@ -1,10 +1,25 @@
 package org.usfirst.frc4904.robot.commands;
 
 
+import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.subsystems.BallIO;
+import org.usfirst.frc4904.standard.commands.RunAllParallel;
+import org.usfirst.frc4904.standard.commands.RunFor;
+import org.usfirst.frc4904.standard.commands.RunUnless;
+import org.usfirst.frc4904.standard.commands.motor.MotorConstant;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class BallioDoorSetIntake extends BallioDoorSet {
+public class BallioDoorSetIntake extends CommandGroup {
 	public BallioDoorSetIntake() {
-		super(BallIO.DoorState.INTAKE);
+		Command hopperRollersDown = new MotorConstant(RobotMap.Component.ballIO.hopperRollers, -0.5);
+		Command intakeRollersUp = new MotorConstant(RobotMap.Component.ballIO.elevatorAndIntakeRoller, 0.5);
+		Command clearServoPath = new RunFor(new RunAllParallel(hopperRollersDown, intakeRollersUp), 0.25);
+		addSequential(new RunUnless(clearServoPath, this::isAlreadySetToIntake));
+		addSequential(new BallioDoorSet(BallIO.DoorState.INTAKE));
+	}
+
+	boolean isAlreadySetToIntake() {
+		return RobotMap.Component.ballIO.getState() == BallIO.DoorState.INTAKE;
 	}
 }
