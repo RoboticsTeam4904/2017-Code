ebaa98ae76e6a9f1885cb868e6968e3313f5be87
Ariana Lissak <ariliss@nuevaschool.org>
2017-02-19 16:03:52 -0800

    fixed gear issue

--- src/org/usfirst/frc4904/robot/autonomous/strategies/AutonPlaceGear.java
+++ src/org/usfirst/frc4904/robot/autonomous/strategies/AutonPlaceGear.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc4904.robot.autonomous.strategies;
 
 import org.usfirst.frc4904.robot.RobotMap;
-import org.usfirst.frc4904.robot.subsystems.GearIO;
+import org.usfirst.frc4904.robot.commands.GearioOuttake;
 import org.usfirst.frc4904.standard.commands.chassis.ChassisMoveDistance;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -10,7 +10,7 @@ public class AutonPlaceGear extends CommandGroup {
 	public AutonPlaceGear() {
 		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis, 38.5, RobotMap.Component.chassisDriveMC,
 				RobotMap.Component.leftWheelEncoder, RobotMap.Component.rightWheelEncoder));
-		RobotMap.Component.gearIO.setState(GearIO.GearState.OUTTAKE);
+		addSequential(new GearioOuttake());
 	}
 
 }
