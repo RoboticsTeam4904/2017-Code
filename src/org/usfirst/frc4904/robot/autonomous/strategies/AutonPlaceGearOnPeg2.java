2e0092eb5a6502966ec4fa90a488aec0da2b3f8f
Ariana Lissak <ariliss@nuevaschool.org>
2017-02-20 12:09:05 -0800

    in formatter

--- src/org/usfirst/frc4904/robot/autonomous/strategies/AutonPlaceGearOnPeg2.java
+++ src/org/usfirst/frc4904/robot/autonomous/strategies/AutonPlaceGearOnPeg2.java
@@ -1,23 +1,23 @@
 package org.usfirst.frc4904.robot.autonomous.strategies;
 
+
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.commands.GearioOuttake;
 import org.usfirst.frc4904.standard.commands.chassis.ChassisMoveDistance;
-
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class AutonPlaceGearOnPeg2 extends CommandGroup {
-	public static double DISTANCE_TO_GEAR2_IN_INCHES = 78.5;
-	public static double DISTANCE_BACK_TO_CLEAR_BOILER = -14.3;
+	public static final double DISTANCE_TO_GEAR2_IN_INCHES = 78.5;
+	public static final double DISTANCE_BACK_TO_CLEAR_BOILER_IN_INCHES = -14.3;
 
 	public AutonPlaceGearOnPeg2() {
 		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis,
-				AutonPlaceGearOnPeg2.DISTANCE_TO_GEAR2_IN_INCHES - 40, RobotMap.Component.chassisDriveMC,
-				RobotMap.Component.leftWheelEncoder, RobotMap.Component.rightWheelEncoder));
+			AutonPlaceGearOnPeg2.DISTANCE_TO_GEAR2_IN_INCHES - 40, RobotMap.Component.chassisDriveMC,
+			RobotMap.Component.leftWheelEncoder, RobotMap.Component.rightWheelEncoder));
 		addSequential(new GearioOuttake());
-		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis, AutonPlaceGearOnPeg2.DISTANCE_BACK_TO_CLEAR_BOILER,
+		addSequential(
+			new ChassisMoveDistance(RobotMap.Component.chassis, AutonPlaceGearOnPeg2.DISTANCE_BACK_TO_CLEAR_BOILER_IN_INCHES,
 				RobotMap.Component.chassisDriveMC, RobotMap.Component.leftWheelEncoder,
 				RobotMap.Component.rightWheelEncoder));
 	}
-
 }
