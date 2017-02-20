1fb76f97bf51d842b5d36460b6570dadf1225f65
Ariana Lissak <ariliss@nuevaschool.org>
2017-02-20 12:47:39 -0800

    fixed variable naming

--- src/org/usfirst/frc4904/robot/autonomous/strategies/AutonPlaceGearOnPeg2.java
+++ src/org/usfirst/frc4904/robot/autonomous/strategies/AutonPlaceGearOnPeg2.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class AutonPlaceGearOnPeg2 extends CommandGroup {
 	public static final double DISTANCE_TO_GEAR2_IN_INCHES = 78.5;
-	public static final double DISTANCE_BACK_TO_CLEAR_BOILER_IN_INCHES = -14.3;
+	public static final double DISTANCE_BACK_TO_CLEAR_PEG_IN_INCHES = -14.3;
 
 	public AutonPlaceGearOnPeg2() {
 		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis,
@@ -16,7 +16,7 @@ public class AutonPlaceGearOnPeg2 extends CommandGroup {
 			RobotMap.Component.leftWheelEncoder, RobotMap.Component.rightWheelEncoder));
 		addSequential(new GearioOuttake());
 		addSequential(
-			new ChassisMoveDistance(RobotMap.Component.chassis, AutonPlaceGearOnPeg2.DISTANCE_BACK_TO_CLEAR_BOILER_IN_INCHES,
+			new ChassisMoveDistance(RobotMap.Component.chassis, AutonPlaceGearOnPeg2.DISTANCE_BACK_TO_CLEAR_PEG_IN_INCHES,
 				RobotMap.Component.chassisDriveMC, RobotMap.Component.leftWheelEncoder,
 				RobotMap.Component.rightWheelEncoder));
 	}
