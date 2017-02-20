0d60b46c520c4202d5201e9e942bea9f7f6fb934
Ariana Lissak <ariliss@nuevaschool.org>
2017-02-20 12:54:39 -0800

    nu,ber save

--- src/org/usfirst/frc4904/robot/autonomous/strategies/AutonPlaceGearShootInLowGoalAndCrossBaseline.java
+++ src/org/usfirst/frc4904/robot/autonomous/strategies/AutonPlaceGearShootInLowGoalAndCrossBaseline.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutonPlaceGearShootInLowGoalAndCrossBaseline extends CommandGroup {
 	public static final double DISTANCE_TO_CLEAR_TURN_RADIUS_IN_INCHES = 10;
 	public static final double DISTANCE_TO_BASELINE_IN_INCHES = 60;
-	public static final double TURN_NEEDED_TO_ALIGN_WITH_BASELINE_IN_DEGREES = 135;
+	public static final double TURN_NEEDED_TO_ALIGN_WITH_BASELINE_IN_DEGREES = -135;
 
 	public AutonPlaceGearShootInLowGoalAndCrossBaseline() {
 		addSequential(new AutonPlaceGearOnPeg2AndShootLowGoal());
