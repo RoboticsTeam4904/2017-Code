08e8524ddc961d816cc7cfa3db0f33a64a680222
Billy <wwpierce020602@gmail.com>
2017-02-19 18:20:22 -0800

    Added CheckMotorConstant function, which will run a motor for a specified time and then let you know, based on the values you give it, whether or not the motor is running the way it should be.

---
+++ src/org/usfirst/frc4904/robot/commands/CheckMotorConstant.java
@@ -0,0 +1,68 @@
+package org.usfirst.frc4904.robot.commands;
+
+
+import org.usfirst.frc4904.standard.LogKitten;
+import org.usfirst.frc4904.standard.custom.sensors.CustomEncoder;
+import org.usfirst.frc4904.standard.subsystems.motor.Motor;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class CheckMotorConstant extends Command { // TODO: Add comments to make clearer.
+	CustomEncoder encoder;
+	Motor motor;
+	Double firstMotorSpeed;
+	Double secondMotorSpeed;
+	Double firstEncoderReading;
+	Double secondEncoderReading;
+	Integer secondsPerReading;
+	Boolean finished;
+	Double allowedError;
+
+	public CheckMotorConstant(Motor motor, CustomEncoder encoder, Double firstMotorSpeed, Double multiplier,
+		Double allowedError, Integer secondsPerReading) {
+		super("Checking motor " + motor.getName());
+		this.encoder = encoder;
+		this.motor = motor;
+		this.firstMotorSpeed = firstMotorSpeed;
+		secondMotorSpeed = firstMotorSpeed * multiplier;
+		this.allowedError = allowedError;
+		this.secondsPerReading = secondsPerReading;
+		finished = false;
+	}
+
+	@Override
+	protected void initialize() {}
+
+	@Override
+	protected void execute() {
+		if (timeSinceInitialized() < secondsPerReading) {
+			motor.set(firstMotorSpeed);
+			firstEncoderReading = encoder.getRate();
+		} else if (timeSinceInitialized() > secondsPerReading && timeSinceInitialized() < secondsPerReading * 2) {
+			motor.set(secondMotorSpeed);
+			secondEncoderReading = encoder.getRate();
+		} else {
+			motor.set(0);
+			if (Math.abs(
+				1 - ((secondMotorSpeed / firstMotorSpeed) / (secondEncoderReading / firstEncoderReading))) < allowedError) {
+				LogKitten.wtf(motor.getName() + " checks successful.");
+			} else {
+				LogKitten.wtf("WARNING: " + motor.getName() + " checks failed.");
+			}
+			finished = true;
+		}
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return finished;
+	}
+
+	@Override
+	protected void end() {}
+
+	@Override
+	protected void interrupted() {}
+}
