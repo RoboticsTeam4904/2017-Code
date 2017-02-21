44d3d7ea76caec98b78027671627951f0a84b096
Yoni Lerner <yonilern@gmail.com>
2017-02-21 09:23:38 -0800

    Fixed Shoot CommandGroup

--- src/org/usfirst/frc4904/robot/commands/Shoot.java
+++ src/org/usfirst/frc4904/robot/commands/Shoot.java
@@ -3,20 +3,17 @@ package org.usfirst.frc4904.robot.commands;
 
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.subsystems.Shooter;
+import org.usfirst.frc4904.standard.commands.RunAllParallel;
 import org.usfirst.frc4904.standard.commands.RunFor;
-import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc4904.standard.commands.WaitUntil;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class Shoot extends CommandGroup {
 	public Shoot() {
 		addSequential(new HopperSetShooter());
-		addParallel(new RunFor(new IndexerUnload(), Shooter.INDEXER_UNLOAD_TIME));
-		addSequential(new Command() {
-			@Override
-			protected boolean isFinished() {
-				return RobotMap.Component.shooter.flywheel.isReady();
-			}
-		});
+		addSequential(new RunAllParallel(
+			new RunFor(new IndexerUnload(), Shooter.INDEXER_UNLOAD_TIME),
+			new WaitUntil(RobotMap.Component.shooter.flywheel::isReady)));
 		addParallel(new IndexerLoad());
 	}
 }
