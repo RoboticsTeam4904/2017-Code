e4517b7f01676c81cb843907895b9d3a93761fa6
Yoni Lerner <yonilern@gmail.com>
2017-02-20 23:31:44 -0800

    Reverted safeties from this branch to make merging quicker
    Still exist on highgoal-safeties

--- src/org/usfirst/frc4904/robot/commands/BallioDoorSetOuttake.java
+++ src/org/usfirst/frc4904/robot/commands/BallioDoorSetOuttake.java
@@ -1,26 +1,10 @@
 package org.usfirst.frc4904.robot.commands;
 
 
-import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.subsystems.BallIO;
-import org.usfirst.frc4904.standard.commands.RunFor;
-import org.usfirst.frc4904.standard.commands.RunUnless;
-import org.usfirst.frc4904.standard.commands.motor.MotorConstant;
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class BallioDoorSetOuttake extends CommandGroup {
-	private static final double SERVO_PATH_CLEAR_TIME = 0.25;
-	private static final double ELEVATOR_CLEAR_SPEED = 0.5;
-
+public class BallioDoorSetOuttake extends BallioDoorSet {
 	public BallioDoorSetOuttake() {
-		Command elevatorsUp = new MotorConstant(RobotMap.Component.ballIO.elevatorAndIntakeRoller, ELEVATOR_CLEAR_SPEED);
-		Command clearServoPath = new RunFor(elevatorsUp, SERVO_PATH_CLEAR_TIME);
-		addSequential(new RunUnless(clearServoPath, this::isAlreadySetToOuttake));
-		addSequential(new BallioDoorSetUnsafely(BallIO.DoorState.OUTTAKE));
-	}
-
-	boolean isAlreadySetToOuttake() {
-		return RobotMap.Component.ballIO.getState() == BallIO.DoorState.OUTTAKE;
+		super(BallIO.DoorState.OUTTAKE);
 	}
 }
