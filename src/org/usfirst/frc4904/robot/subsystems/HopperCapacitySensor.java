625274669cee5a356d15e99608f64456a93cb32b
Yoni Lerner <yonilern@gmail.com>
2017-02-24 12:09:34 -0800

    Removed unnecessary comment

--- src/org/usfirst/frc4904/robot/subsystems/HopperCapacitySensor.java
+++ src/org/usfirst/frc4904/robot/subsystems/HopperCapacitySensor.java
@@ -2,7 +2,6 @@ package org.usfirst.frc4904.robot.subsystems;
 
 
 import org.usfirst.frc4904.robot.RobotMap;
-// import org.usfirst.frc4904.standard.LogKitten;
 import org.usfirst.frc4904.standard.custom.sensors.CANSensor;
 import org.usfirst.frc4904.standard.custom.sensors.InvalidSensorException;
 
@@ -11,10 +10,6 @@ public class HopperCapacitySensor extends CANSensor {
 		super("HopperCapacitySensor", RobotMap.Port.CAN.hopperSensor);
 	}
 
-	// public double getCapacity() throws InvalidSensorException {
-	// LogKitten.wtf("Capacity sensor value: " + super.readSensor().toString());
-	// return 0.0;
-	// }
 	public double getCapacity() throws InvalidSensorException {
 		double rightBalls;
 		double leftBalls;
