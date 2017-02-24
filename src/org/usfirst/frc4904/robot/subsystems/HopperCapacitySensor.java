0747da11119cc8aed33e1cc9e215620df484ab66
Yoni Lerner <yonilern@gmail.com>
2017-02-24 12:21:27 -0800

    Moved constants to static properties

--- src/org/usfirst/frc4904/robot/subsystems/HopperCapacitySensor.java
+++ src/org/usfirst/frc4904/robot/subsystems/HopperCapacitySensor.java
@@ -5,6 +5,11 @@ import org.usfirst.frc4904.standard.custom.sensors.CANSensor;
 import org.usfirst.frc4904.standard.custom.sensors.InvalidSensorException;
 
 public class HopperCapacitySensor extends CANSensor {
+	protected static final double BALLS_PER_LAYER = 12; // actually 14.3 but balls won't land perfectly
+	protected static final double SENSOR_HEIGHT_ABOVE_HOPPER_INCHES = 36.8;
+	protected static final double MAX_SENSOR_VALUE_INCHES = 83.82;
+	protected static final double BALL_HEIGHT_INCHES = 12.7;
+
 	public HopperCapacitySensor(int port) {
 		super("HopperCapacitySensor", port);
 	}
@@ -12,22 +17,24 @@ public class HopperCapacitySensor extends CANSensor {
 	public double getCapacity() throws InvalidSensorException {
 		double rightBalls;
 		double leftBalls;
-		double ballsperlayer = 12; // actually 14.3 but balls won't land perfectly
 		double averageBalls;
 		double teensyOutputLeft;
 		double teensyOutputRight;
 		// final double hopperHeight = 41.9;
-		final double sensorHeight = 36.8;
-		final double hypotunuseLength = 83.82;
-		final double ballHeight = 12.7;
 		double output;
 		teensyOutputLeft = (readSensor()[0]) / 100.0;
 		teensyOutputRight = (readSensor()[1]) / 100.0;
 		// divide by 100 to get accurate ball
-		rightBalls = (sensorHeight * (hypotunuseLength - teensyOutputLeft) / hypotunuseLength) / ballHeight;
-		leftBalls = (sensorHeight * (hypotunuseLength - teensyOutputRight) / hypotunuseLength) / ballHeight;
+		rightBalls = (HopperCapacitySensor.SENSOR_HEIGHT_ABOVE_HOPPER_INCHES
+			* (HopperCapacitySensor.MAX_SENSOR_VALUE_INCHES - teensyOutputLeft)
+			/ HopperCapacitySensor.MAX_SENSOR_VALUE_INCHES)
+			/ HopperCapacitySensor.BALL_HEIGHT_INCHES;
+		leftBalls = (HopperCapacitySensor.SENSOR_HEIGHT_ABOVE_HOPPER_INCHES
+			* (HopperCapacitySensor.MAX_SENSOR_VALUE_INCHES - teensyOutputRight)
+			/ HopperCapacitySensor.MAX_SENSOR_VALUE_INCHES)
+			/ HopperCapacitySensor.BALL_HEIGHT_INCHES;
 		averageBalls = (rightBalls + leftBalls) / 2;
-		output = ballsperlayer * averageBalls;
+		output = HopperCapacitySensor.BALLS_PER_LAYER * averageBalls;
 		return output;
 	}
 }
