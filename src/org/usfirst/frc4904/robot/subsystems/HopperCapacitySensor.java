6e3148d25075bab99071cce2b9ac46d885b96f33
Yoni Lerner <yonilern@gmail.com>
2017-02-24 13:10:12 -0800

    Generalized calculation to n sensors

--- src/org/usfirst/frc4904/robot/subsystems/HopperCapacitySensor.java
+++ src/org/usfirst/frc4904/robot/subsystems/HopperCapacitySensor.java
@@ -10,6 +10,7 @@ public class HopperCapacitySensor extends CANSensor {
 	protected static final double MAX_SENSOR_VALUE_INCHES = 83.82;
 	protected static final double BALL_HEIGHT_INCHES = 12.7;
 	protected static final double TEENSY_OUTPUT_TO_INCHES_CONVERSION_FACTOR = 1.0 / 100.0;
+	protected static final int NUM_SENSORS = 2;
 
 	public HopperCapacitySensor(int port) {
 		super("HopperCapacitySensor", port);
@@ -17,14 +18,14 @@ public class HopperCapacitySensor extends CANSensor {
 
 	public double getCapacity() throws InvalidSensorException {
 		int[] rawSensorOutput = readSensor();
-		double sensorOutputLeft = rawSensorOutput[0] * HopperCapacitySensor.TEENSY_OUTPUT_TO_INCHES_CONVERSION_FACTOR;
-		double sensorOutputRight = rawSensorOutput[1] * HopperCapacitySensor.TEENSY_OUTPUT_TO_INCHES_CONVERSION_FACTOR;
-		double leftBalls = HopperCapacitySensor.SENSOR_HEIGHT_ABOVE_HOPPER_INCHES
-			* (1 - (sensorOutputLeft / HopperCapacitySensor.MAX_SENSOR_VALUE_INCHES)) / HopperCapacitySensor.BALL_HEIGHT_INCHES;
-		double rightBalls = HopperCapacitySensor.SENSOR_HEIGHT_ABOVE_HOPPER_INCHES
-			* (1 - (sensorOutputRight / HopperCapacitySensor.MAX_SENSOR_VALUE_INCHES))
-			/ HopperCapacitySensor.BALL_HEIGHT_INCHES;
-		double averageBalls = (rightBalls + leftBalls) / 2;
+		double ballsSum = 0;
+		for (int i = 0; i < HopperCapacitySensor.NUM_SENSORS; i++) {
+			double sensorOutput = rawSensorOutput[i] * HopperCapacitySensor.TEENSY_OUTPUT_TO_INCHES_CONVERSION_FACTOR;
+			double balls = HopperCapacitySensor.SENSOR_HEIGHT_ABOVE_HOPPER_INCHES
+				* (1 - (sensorOutput / HopperCapacitySensor.MAX_SENSOR_VALUE_INCHES)) / HopperCapacitySensor.BALL_HEIGHT_INCHES;
+			ballsSum += balls;
+		}
+		double averageBalls = ballsSum / HopperCapacitySensor.NUM_SENSORS;
 		double output = HopperCapacitySensor.BALLS_PER_LAYER * averageBalls;
 		return output;
 	}
