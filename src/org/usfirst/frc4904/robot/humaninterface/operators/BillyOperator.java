3c88d54e6180c80b48a4ec5a04416e289e78c5d0
Billy <wwpierce020602@gmail.com>
2017-03-05 13:26:51 -0800

    Changed buttons

--- src/org/usfirst/frc4904/robot/humaninterface/operators/BillyOperator.java
+++ src/org/usfirst/frc4904/robot/humaninterface/operators/BillyOperator.java
@@ -32,7 +32,8 @@ public class BillyOperator extends Operator {
 		RobotMap.Component.operatorStick.button5.onlyWhileHeld(new BallioFloorClear());
 		RobotMap.Component.operatorStick.button6.onlyWhileHeld(new BallioCycle());
 		RobotMap.Component.operatorStick.button7.onlyWhileHeld(new BallioFloorClear());
-		// RobotMap.Component.operatorStick.button8.onlyWhileHeld(new BallioOuttake());
+		RobotMap.Component.operatorStick.button8.onlyWhileHeld(new BallioOuttake());
+		RobotMap.Component.operatorStick.button10.onlyWhileHeld(new BallioIntake());
 		RobotMap.Component.operatorStick.button8.onlyWhileHeld(new BallioIntake());
 		new ThresholdCommand<Double>(new GearioIntake(), RobotMap.Component.operatorStick::getY,
 			BillyOperator.INTAKE_THRESHOLD)
