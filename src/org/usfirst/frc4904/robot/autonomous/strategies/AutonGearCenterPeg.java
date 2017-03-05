741ebad1c772ea4d1ab41294587e52667726af54
Carter Turnbaugh <carturn@nuevaschool.org>
2017-03-05 13:50:48 -0800

    Updated constants

--- src/org/usfirst/frc4904/robot/autonomous/strategies/AutonGearCenterPeg.java
+++ src/org/usfirst/frc4904/robot/autonomous/strategies/AutonGearCenterPeg.java
@@ -7,12 +7,16 @@ import org.usfirst.frc4904.standard.commands.chassis.ChassisMoveDistance;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class AutonGearCenterPeg extends CommandGroup {
-	public static final double DISTANCE_TO_GEAR2_IN_INCHES = 78.5;
-	public static final double DISTANCE_BACK_TO_CLEAR_PEG_IN_INCHES = -14.3;
+	public static final double DISTANCE_TO_GEAR2_IN_INCHES = 114.3;
+	public static final double ROBOT_LENGTH_IN_INCHES = 40;
+	// Geario is in the back, invert values
+	public static final double DISTANCE_BACK_TO_CLEAR_PEG_IN_INCHES = 14.3;
+	public static final double DISTANCE_TO_DRIVE_IN_INCHES = -1
+		* (AutonGearCenterPeg.DISTANCE_TO_GEAR2_IN_INCHES - AutonGearCenterPeg.ROBOT_LENGTH_IN_INCHES);
 
 	public AutonGearCenterPeg() {
 		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis,
-			AutonGearCenterPeg.DISTANCE_TO_GEAR2_IN_INCHES - 40, RobotMap.Component.chassisDriveMC,
+			AutonGearCenterPeg.DISTANCE_TO_DRIVE_IN_INCHES, RobotMap.Component.chassisDriveMC,
 			RobotMap.Component.leftWheelEncoder, RobotMap.Component.rightWheelEncoder));
 		addSequential(new GearioOuttake());
 		addSequential(
