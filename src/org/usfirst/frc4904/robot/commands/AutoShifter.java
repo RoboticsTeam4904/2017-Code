735a0175000ffb90d80bebebdd13e32355a297cb
Yoni Lerner <yonilern@gmail.com>
2017-03-08 17:09:14 -0800

    Simplified auto shifter

--- src/org/usfirst/frc4904/robot/commands/AutoShifter.java
+++ src/org/usfirst/frc4904/robot/commands/AutoShifter.java
@@ -19,11 +19,9 @@ public class AutoShifter extends Command {
 	public static final double LAST_MANUAL_SHIFT_TIME_MILLIS = 5000;
 	public static final double LAST_AUTO_SHIFT_TIME_MILLIS = 250;
 	public static final double MAX_WHEEL_SPEED_VALUE_DIFFERENCE_INDICATING_STRAIGHT_MOTION = 0.5;
-	public static final double SLOW_SPEED_THRESHOLD = 24; // inches per sec, according to the encoders
 	public static final double MEDIUM_SPEED_THRESHOLD = 40;
-	public static final double SLOW_THROTTLE_THRESHOLD = 0.4;
 	public static final double MEDIUM_THROTTLE_THRESHOLD = 0.5;
-	public static final double FAST_THROTTLE_THRESHOLD = 0.8;
+	public static final double FAST_THROTTLE_THRESHOLD = 0.75;
 	protected final CustomEncoder leftEncoder;
 	protected final CustomEncoder rightEncoder;
 	protected final AutoSolenoidShifters shifter;
@@ -50,35 +48,21 @@ public class AutoShifter extends Command {
 		if (hasManuallyShiftedRecently || hasAutomaticallyShiftedRecently) {
 			return;
 		}
-		// Figure out whether the code wants the robot to go straight by looking at the difference between motor speeds of both sides.
 		double[] motorThrottles = RobotMap.Component.chassis.getMotorSpeeds();
-		boolean isNotGoingStraight = Math.abs(
-			motorThrottles[0] - motorThrottles[1]) > AutoShifter.MAX_WHEEL_SPEED_VALUE_DIFFERENCE_INDICATING_STRAIGHT_MOTION;
-		// If the robot is trying to turn (sharply), shift down (the chassis can't turn well in high gear).
-		if (isNotGoingStraight) {
-			LogKitten.v("Downshifting to make turning easier.");
-			shiftDownCommand.start();
-			return;
-		}
 		// Calculate the average of the encoder speeds, which is the same as the overall actual forward speed because the turn speed is added to one side and subtracted from the other.
 		// Also, take the absolute value to treat forwards & backwards the same.
 		double absoluteForwardSpeed = Math.abs((leftEncoder.getRate() + rightEncoder.getRate()) / 2);
 		// Calculate the average of all motor speeds, which is the same as the overall throttle (desired forward speed) because the turn speed is added to one side and subtracted from the other.
 		// Also, take the absolute value to treat forwards & backwards the same.
 		double absoluteThrottle = Math.abs(Arrays.stream(motorThrottles).average().getAsDouble());
-		// If we're flooring it and nothing's in our way, shift up.
-		boolean isAboveMediumSpeed = absoluteForwardSpeed > AutoShifter.MEDIUM_SPEED_THRESHOLD;
-		boolean isThrottleFast = absoluteThrottle > AutoShifter.FAST_THROTTLE_THRESHOLD;
-		if (isAboveMediumSpeed && isThrottleFast) {
-			LogKitten.v("Upshifting to allow the driver to floor it.");
-			shiftUpCommand.start();
-			return;
-		}
-		// If we're just driving very slowly, shift down.
-		boolean isSpeedSlow = absoluteForwardSpeed < AutoShifter.SLOW_SPEED_THRESHOLD;
-		boolean isThrottleLessThanSlow = absoluteThrottle < AutoShifter.SLOW_THROTTLE_THRESHOLD;
-		if (isSpeedSlow && isThrottleLessThanSlow) {
-			LogKitten.v("Downshift for slow driving.");
+		// Figure out whether the code wants the robot to go straight by looking at the difference between motor speeds of both sides.
+		boolean isTurning = Math.abs(
+			motorThrottles[0] - motorThrottles[1]) > AutoShifter.MAX_WHEEL_SPEED_VALUE_DIFFERENCE_INDICATING_STRAIGHT_MOTION;
+		boolean isNotAboveFastThrottle = absoluteThrottle > AutoShifter.FAST_THROTTLE_THRESHOLD;
+		// If the robot is trying to turn (sharply) in place, shift down (the chassis can't turn well in high gear).
+		// (Don't do this if we're throttling it fast, to allow drifting and big-radius turns)
+		if (isTurning && isNotAboveFastThrottle) {
+			LogKitten.v("Downshifting to make turning easier.");
 			shiftDownCommand.start();
 			return;
 		}
