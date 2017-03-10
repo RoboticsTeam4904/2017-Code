72cd25f012eee1f3e212c92341d61128936c86b2
Yoni Lerner <yonilern@gmail.com>
2017-03-09 19:49:06 -0800

    Refactored auton routines

--- src/org/usfirst/frc4904/robot/autonomous/strategies/AutonGearBoilerPeg.java
+++ src/org/usfirst/frc4904/robot/autonomous/strategies/AutonGearBoilerPeg.java
@@ -7,20 +7,15 @@ import org.usfirst.frc4904.standard.commands.chassis.ChassisMoveDistance;
 import org.usfirst.frc4904.standard.commands.chassis.ChassisTurn;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class AutonPlaceGearOnPeg1BlueAndPeg3Red extends CommandGroup {
-	public static final double MOVE_BASE_IN_INCHES_1 = 93.3;
-	public static final double MOVE_BASE_IN_INCHES_2 = 120;
-	public static final int TURN_BASE_IN_DEGREES_1 = 46;
-
-	public AutonPlaceGearOnPeg1BlueAndPeg3Red() {
+public class AutonGearBoilerPeg extends CommandGroup {
+	public AutonGearBoilerPeg() {
 		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis,
-			AutonPlaceGearOnPeg1BlueAndPeg3Red.MOVE_BASE_IN_INCHES_1, RobotMap.Component.chassisDriveMC));
+			AutonGearLoadPeg.MOVE_BASE_IN_INCHES_1, RobotMap.Component.chassisDriveMC));
 		addSequential(
-			new ChassisTurn(RobotMap.Component.chassis,
-				AutonPlaceGearOnPeg1BlueAndPeg3Red.TURN_BASE_IN_DEGREES_1,
+			new ChassisTurn(RobotMap.Component.chassis, -1 * (AutonGearLoadPeg.TURN_BASE_IN_DEGREES_1),
 				RobotMap.Component.navx, RobotMap.Component.chassisTurnMC));
 		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis,
-			AutonPlaceGearOnPeg1BlueAndPeg3Red.MOVE_BASE_IN_INCHES_2, RobotMap.Component.chassisDriveMC));
+			AutonGearLoadPeg.MOVE_BASE_IN_INCHES_2, RobotMap.Component.chassisDriveMC));
 		addSequential(new GearioOuttake());
 		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis,
 			AutonGearCenterPeg.DISTANCE_BACK_TO_CLEAR_PEG_IN_INCHES, RobotMap.Component.chassisDriveMC));
--- src/org/usfirst/frc4904/robot/autonomous/strategies/AutonGearLoadPeg.java
+++ src/org/usfirst/frc4904/robot/autonomous/strategies/AutonGearLoadPeg.java
@@ -7,15 +7,20 @@ import org.usfirst.frc4904.standard.commands.chassis.ChassisMoveDistance;
 import org.usfirst.frc4904.standard.commands.chassis.ChassisTurn;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class AutonPlaceGearOnPeg1RedAndPeg3Blue extends CommandGroup {
-	public AutonPlaceGearOnPeg1RedAndPeg3Blue() {
+public class AutonGearLoadPeg extends CommandGroup {
+	public static final double MOVE_BASE_IN_INCHES_1 = 93.3;
+	public static final double MOVE_BASE_IN_INCHES_2 = 120;
+	public static final int TURN_BASE_IN_DEGREES_1 = 46;
+
+	public AutonGearLoadPeg() {
 		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis,
-			AutonPlaceGearOnPeg1BlueAndPeg3Red.MOVE_BASE_IN_INCHES_1, RobotMap.Component.chassisDriveMC));
+			AutonGearLoadPeg.MOVE_BASE_IN_INCHES_1, RobotMap.Component.chassisDriveMC));
 		addSequential(
-			new ChassisTurn(RobotMap.Component.chassis, -1 * (AutonPlaceGearOnPeg1BlueAndPeg3Red.TURN_BASE_IN_DEGREES_1),
+			new ChassisTurn(RobotMap.Component.chassis,
+				AutonGearLoadPeg.TURN_BASE_IN_DEGREES_1,
 				RobotMap.Component.navx, RobotMap.Component.chassisTurnMC));
 		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis,
-			AutonPlaceGearOnPeg1BlueAndPeg3Red.MOVE_BASE_IN_INCHES_2, RobotMap.Component.chassisDriveMC));
+			AutonGearLoadPeg.MOVE_BASE_IN_INCHES_2, RobotMap.Component.chassisDriveMC));
 		addSequential(new GearioOuttake());
 		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis,
 			AutonGearCenterPeg.DISTANCE_BACK_TO_CLEAR_PEG_IN_INCHES, RobotMap.Component.chassisDriveMC));
