4b3326c9fe0590ac7451e8fe35e2e21a15e8c975
Yoni Lerner <yonilern@gmail.com>
2017-03-10 08:57:31 -0800

    Moved WiggleApproach to its own file
    And added the option to pass your own parameters

--- src/org/usfirst/frc4904/robot/autonomous/strategies/AutonGearCenterPegTime.java
+++ src/org/usfirst/frc4904/robot/autonomous/strategies/AutonGearCenterPegTime.java
@@ -7,7 +7,6 @@ import org.usfirst.frc4904.standard.commands.RunAllSequential;
 import org.usfirst.frc4904.standard.commands.RunFor;
 import org.usfirst.frc4904.standard.commands.chassis.ChassisConstant;
 import org.usfirst.frc4904.standard.commands.chassis.ChassisMove;
-import org.usfirst.frc4904.standard.custom.ChassisController;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
@@ -15,10 +14,7 @@ public class AutonGearCenterPegTime extends CommandGroup {
 	public static final double TIME_BACK_TO_CLEAR_PEG = 1;
 	public static final double DRIVE_SPEED = -0.6;
 	public static final double TIME_INITIAL_APPROACH = 1.5;
-	public static final double WIGGLE_SPEED = -0.3;
 	public static final double WIGGLE_TIME = 2.0; // It's wiggle time!
-	public static final double WIGGLE_AMPLITUDE = 0.5;
-	public static final double WIGGLE_PERIOD = 1.0 / 64.0;
 
 	public AutonGearCenterPegTime() {
 		addSequential(new ChassisConstant(RobotMap.Component.chassis, 0, AutonGearCenterPegTime.DRIVE_SPEED, 0,
@@ -29,23 +25,5 @@ public class AutonGearCenterPegTime extends CommandGroup {
 		addParallel(new RunAllSequential(new WaitCommand(1),
 			new ChassisConstant(RobotMap.Component.chassis, 0, -AutonGearCenterPegTime.DRIVE_SPEED, 0,
 				AutonGearCenterPegTime.TIME_BACK_TO_CLEAR_PEG)));
-	}
-
-	public class WiggleApproach implements ChassisController {
-		@Override
-		public double getX() {
-			return 0;
-		}
-
-		@Override
-		public double getY() {
-			return AutonGearCenterPegTime.WIGGLE_SPEED;
-		}
-
-		@Override
-		public double getTurnSpeed() {
-			return Math.sin(System.currentTimeMillis() * AutonGearCenterPegTime.WIGGLE_PERIOD)
-				* AutonGearCenterPegTime.WIGGLE_AMPLITUDE;
-		}
 	}
 }
