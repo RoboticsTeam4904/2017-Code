ff57e039574e4ba2fed139fa5f0b1a12b9723c62
Yoni Lerner <yonilern@gmail.com>
2017-03-11 14:44:18 -0800

    Added javadocs

--- src/org/usfirst/frc4904/robot/commands/OverridableCommand.java
+++ src/org/usfirst/frc4904/robot/commands/OverridableCommand.java
@@ -6,11 +6,22 @@ import edu.wpi.first.wpilibj.command.Command;
 public abstract class OverridableCommand extends Command implements Overridable {
 	private volatile boolean isOverridden;
 
+	/**
+	 * Set whether this command is overridden.
+	 * 
+	 * @param isOverridden
+	 *        Whether to override the command or not
+	 */
 	@Override
 	final public synchronized void setOverridden(boolean isOverridden) {
 		this.isOverridden = isOverridden;
 	}
 
+	/**
+	 * Get whether this command is overridden.
+	 * 
+	 * @returns Whether this command is overridden.
+	 */
 	@Override
 	final public synchronized boolean isOverridden() {
 		return isOverridden;
