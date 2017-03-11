6c482e13bc2185c5adf8f073a486cd6e8cafddd7
Yoni Lerner <yonilern@gmail.com>
2017-03-11 13:48:59 -0800

    Fixed a bunch of stuff

--- src/org/usfirst/frc4904/robot/commands/CalibrateCameraExposure.java
+++ src/org/usfirst/frc4904/robot/commands/CalibrateCameraExposure.java
@@ -1,29 +1,31 @@
 package org.usfirst.frc4904.robot.commands;
 
 
+import org.usfirst.frc4904.robot.SmartDashboardKey;
+import org.usfirst.frc4904.standard.LogKitten;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class CalibrateCameraExposure extends Command {
 	protected final NetworkTable table;
-	protected double numCalibrations;
 
 	public CalibrateCameraExposure() {
-		table = NetworkTable.getTable("autocalibrate");
+		table = NetworkTable.getTable("Vision");
 	}
 
 	@Override
 	protected void initialize() {
-		numCalibrations = 0;
-	}
-
-	@Override
-	protected void execute() {
-		table.putNumber("autocalibrate", ++numCalibrations);
+		table.putBoolean(SmartDashboardKey.VISION_AUTOCALIBRATION.key, true);
+		table.putBoolean(SmartDashboardKey.VISION_AUTOCALIBRATION_COMPLETE.key, false);
 	}
 
 	@Override
 	protected boolean isFinished() {
-		return table.getBoolean("didCalibrate", false);
+		return table.getBoolean(SmartDashboardKey.VISION_AUTOCALIBRATION_COMPLETE.key, false);
+	}
+
+	@Override
+	protected void end() {
+		LogKitten.w("Autocalibration complete");
 	}
 }
