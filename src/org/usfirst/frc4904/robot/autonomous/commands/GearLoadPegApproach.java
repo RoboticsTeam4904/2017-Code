006a6e611247e91b5f32aea953410bf25cb257d7
AJ Nadel <aj@ajnadel.com>
2017-03-11 15:36:16 -0800

    don't use inches for time (whoops)

--- src/org/usfirst/frc4904/robot/autonomous/commands/GearLoadPegApproach.java
+++ src/org/usfirst/frc4904/robot/autonomous/commands/GearLoadPegApproach.java
@@ -12,18 +12,18 @@ public class GearLoadPegApproach extends CommandGroup {
 	public static final double TIME_INITIAL_APPROACH_1 = 2;
 	public static final double TIME_TURN = 1;
 	public static final double TIME_INITIAL_APPROACH_2 = 0.75;
-	public static final double INCHES_INITIAL_APPROACH_1 = 100;
+	public static final double INCHES_INITIAL_APPROACH_1 = -100;
 	public static final double DEGREES_TURN = 60;
-	public static final double INCHES_INITIAL_APPROACH_2 = 69;
+	public static final double INCHES_INITIAL_APPROACH_2 = -69;
 
 	public GearLoadPegApproach(boolean useSensors) {
 		ChassisConstant timeMoveA = new ChassisConstant(RobotMap.Component.chassis, 0, AutonConfig.DEAD_RECKON_DRIVE_SPEED,
-			0, GearLoadPegApproach.INCHES_INITIAL_APPROACH_1);
+			0, GearLoadPegApproach.TIME_INITIAL_APPROACH_1);
 		ChassisConstant timeTurn = new ChassisConstant(RobotMap.Component.chassis, 0, 0,
 			AutonConfig.DEAD_RECKON_TURN_SPEED * AutonConfig.ALLIANCE_FACTOR, GearLoadPegApproach.TIME_TURN);
 		ChassisConstant timeMoveB = new ChassisConstant(RobotMap.Component.chassis, 0, AutonConfig.DEAD_RECKON_DRIVE_SPEED,
 			0,
-			GearLoadPegApproach.INCHES_INITIAL_APPROACH_2);
+			GearLoadPegApproach.TIME_INITIAL_APPROACH_2);
 		if (useSensors) {
 			addSequential(new ChassisMoveDistance(RobotMap.Component.chassis, GearLoadPegApproach.INCHES_INITIAL_APPROACH_1,
 				RobotMap.Component.chassisDriveMC, timeMoveA));
