43e52a0b6aa1737bedd485a63aeae37a4ac93750
Carter Turnbaugh <carturn@nuevaschool.org>
2017-03-11 17:55:50 -0800

    Removed duplicated commands

--- src/org/usfirst/frc4904/robot/commands/BallioCycle.java
+++ src/org/usfirst/frc4904/robot/commands/BallioCycle.java
@@ -10,13 +10,10 @@ public class BallioCycle extends OverridableCommandGroup {
 		requires(RobotMap.Component.ballIO);
 		addSequential(new BallioDoorSetOuttake());
 		addSequentialUnlessOverridden(new HopperSetBallio());
-		addParallel(new MotorConstant(RobotMap.Component.ballIO.directionalRoller, 1.0));
-		addParallel(new MotorConstant(RobotMap.Component.ballIO.elevatorAndIntakeRoller, 1.0));
-		addSequential(new MotorConstant(RobotMap.Component.ballIO.hopperRollers, 1.0));
-		addSequentialUnlessOverridden(new HopperSetBallio());
 		addParallel(new MotorConstant(RobotMap.Component.ballIO.directionalRoller, BallIO.DIRECTIONAL_ROLLER_INTAKE_SPEED));
 		addParallel(new MotorConstant(RobotMap.Component.ballIO.elevatorAndIntakeRoller,
 			BallIO.ELEVATOR_AND_INTAKE_ROLLER_FORWARD_SPEED));
 		addSequential(new MotorConstant(RobotMap.Component.ballIO.hopperRollers, BallIO.HOPPER_ROLLERS_FORWARD_SPEED));
+		addSequentialUnlessOverridden(new HopperSetBallio());
 	}
 }
