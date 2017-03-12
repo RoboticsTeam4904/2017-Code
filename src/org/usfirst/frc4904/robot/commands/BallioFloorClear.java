d90ed4e3b5c53fe7af29b93828ca2e653439a376
Carter Turnbaugh <carturn@nuevaschool.org>
2017-03-12 13:36:04 -0700

    Added hopper overrides to more command groups

--- src/org/usfirst/frc4904/robot/commands/BallioFloorClear.java
+++ src/org/usfirst/frc4904/robot/commands/BallioFloorClear.java
@@ -4,14 +4,13 @@ package org.usfirst.frc4904.robot.commands;
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.subsystems.BallIO;
 import org.usfirst.frc4904.standard.commands.motor.MotorConstant;
-import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class BallioFloorClear extends CommandGroup {
+public class BallioFloorClear extends OverridableCommandGroup {
 	public BallioFloorClear() {
-		super("BallioFloorClear");
+		super("BallioFloorClear", RobotMap.Component.hopper);
 		requires(RobotMap.Component.ballIO);
 		addSequential(new BallioDoorSetIntake());
-		addSequential(new HopperSetBallio());
+		addSequentialUnlessOverridden(new HopperSetBallio());
 		addParallel(new MotorConstant(RobotMap.Component.ballIO.directionalRoller, BallIO.DIRECTIONAL_ROLLER_OUTTAKE_SPEED));
 		addParallel(new MotorConstant(RobotMap.Component.ballIO.elevatorAndIntakeRoller,
 			BallIO.ELEVATOR_AND_INTAKE_ROLLER_FORWARD_SPEED));
