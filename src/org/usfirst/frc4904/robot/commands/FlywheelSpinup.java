86a4fa5895f7dd068093a80f6b3ce7b3aa73254e
Yoni Lerner <yonilern@gmail.com>
2017-03-13 09:41:13 -0700

    Reverted unused changes to FlywheelSpinup

--- src/org/usfirst/frc4904/robot/commands/FlywheelSpinup.java
+++ src/org/usfirst/frc4904/robot/commands/FlywheelSpinup.java
@@ -1,19 +1,12 @@
 package org.usfirst.frc4904.robot.commands;
 
 
-import java.util.function.Supplier;
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.subsystems.Flywheel;
 import org.usfirst.frc4904.standard.commands.motor.MotorConstant;
 
 public class FlywheelSpinup extends MotorConstant {
-	protected static boolean flywheelSpeedOverride = false;
-
 	public FlywheelSpinup() {
 		super("FlywheelSpinup", RobotMap.Component.flywheel, Flywheel.SHOOTING_SPEED);
-	}
-
-	public FlywheelSpinup(Supplier<Double> speedSupplier) {
-		super(RobotMap.Component.flywheel, speedSupplier.get());
 	}
 }
