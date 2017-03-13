ad44b41cd2fdf340f664ce8e7f8f43ace4684793
Yoni Lerner <yonilern@gmail.com>
2017-03-13 09:32:18 -0700

    Fixed DefaultOperator

--- src/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
+++ src/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc4904.robot.humaninterface.operators;
 
 
-import java.util.function.Supplier;
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.commands.BallioCycle;
 import org.usfirst.frc4904.robot.commands.BallioFloorClear;
@@ -22,9 +21,9 @@ import org.usfirst.frc4904.standard.commands.OverrideDisable;
 import org.usfirst.frc4904.standard.commands.OverrideEnable;
 import org.usfirst.frc4904.standard.commands.RunIfElse;
 import org.usfirst.frc4904.standard.commands.SingleOp;
+import org.usfirst.frc4904.standard.commands.ThresholdCommand;
 import org.usfirst.frc4904.standard.commands.motor.speedmodifiers.SetEnableableModifier;
 import org.usfirst.frc4904.standard.humaninput.Operator;
-import edu.wpi.first.wpilibj.command.Command;
 
 public class DefaultOperator extends Operator {
 	public static final double INTAKE_THRESHOLD = 0.5;
@@ -55,9 +54,9 @@ public class DefaultOperator extends Operator {
 		RobotMap.Component.operatorStick.button10.whenPressed(new SetRampState(GearIO.RampState.EXTENDED));
 		RobotMap.Component.operatorStick.button11.whenPressed(new HopperSetShooter());
 		RobotMap.Component.operatorStick.button12.whenPressed(new HopperSetBallio());
-		new ThresholdCommand(new GearioIntake(), RobotMap.Component.operatorStick::getY,
+		new ThresholdCommand<Double>(new GearioIntake(), RobotMap.Component.operatorStick::getY,
 			DefaultOperator.INTAKE_THRESHOLD).start();
-		new ThresholdCommand(new GearioOuttake(), RobotMap.Component.operatorStick::getY,
+		new ThresholdCommand<Double>(new GearioOuttake(), RobotMap.Component.operatorStick::getY,
 			-DefaultOperator.INTAKE_THRESHOLD, true).start();
 		new SetRampState(GearIO.RampState.EXTENDED).start();
 		RobotMap.Component.teensyStick.getButton(2).whenPressed(new OverrideEnable(RobotMap.Component.flywheel));
@@ -81,41 +80,5 @@ public class DefaultOperator extends Operator {
 			.whenPressed(new SingleOp(RobotMap.Component.flywheel::disableMotionController));
 		RobotMap.Component.teensyStick.getButton(16)
 			.whenReleased(new SingleOp(RobotMap.Component.flywheel::disableMotionController));
-	}
-
-	private class ThresholdCommand extends Command {
-		protected final Command command;
-		protected final Supplier<Double> axis;
-		protected final double threshold;
-		protected final boolean invert;
-
-		public ThresholdCommand(Command command, Supplier<Double> axis, double threshold, boolean invert) {
-			this.command = command;
-			this.axis = axis;
-			this.threshold = threshold;
-			this.invert = invert;
-		}
-
-		public ThresholdCommand(Command command, Supplier<Double> axis, double threshold) {
-			this(command, axis, threshold, false);
-		}
-
-		protected boolean pastThreshold(double value) {
-			return (!invert && value >= threshold) || (invert && value <= threshold);
-		}
-
-		@Override
-		protected void execute() {
-			if (pastThreshold(axis.get()) && !command.isRunning()) {
-				command.start();
-			} else if (!pastThreshold(axis.get()) && command.isRunning()) {
-				command.cancel();
-			}
-		}
-
-		@Override
-		protected boolean isFinished() {
-			return false;
-		}
 	}
 }
