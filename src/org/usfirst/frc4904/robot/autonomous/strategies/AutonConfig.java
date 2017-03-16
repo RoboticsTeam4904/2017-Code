46408973785327f6979e048a7914ce945a579048
Yoni Lerner <yonilern@gmail.com>
2017-03-16 10:06:32 -0700

    Fixed ALLIANCE_FACTOR calculation

--- src/org/usfirst/frc4904/robot/autonomous/strategies/AutonConfig.java
+++ src/org/usfirst/frc4904/robot/autonomous/strategies/AutonConfig.java
@@ -4,7 +4,8 @@ package org.usfirst.frc4904.robot.autonomous.strategies;
 import edu.wpi.first.wpilibj.DriverStation;
 
 public class AutonConfig {
-	public static final double ALLIANCE_FACTOR = DriverStation.getInstance().getAlliance() == DriverStation.Alliance.Blue ? 1.0
+	public static final double ALLIANCE_FACTOR = DriverStation.getInstance().getAlliance().equals(DriverStation.Alliance.Blue)
+		? 1.0
 		: -1.0;
 	public static final double DEAD_RECKON_OUTTAKE_TIME = 1.5;
 	public static final double DEAD_RECKON_TIME_BACK_TO_CLEAR_PEG = 1.0;
