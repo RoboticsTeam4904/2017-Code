54b91aac8d8a80b22dd2f7436fddf046bc063da2
Yoni Lerner <yonilern@gmail.com>
2017-03-16 10:11:38 -0700

    Slowed down backoff after outtake and changed outtake times

--- src/org/usfirst/frc4904/robot/autonomous/strategies/AutonConfig.java
+++ src/org/usfirst/frc4904/robot/autonomous/strategies/AutonConfig.java
@@ -7,9 +7,10 @@ public class AutonConfig {
 	public static final double ALLIANCE_FACTOR = DriverStation.getInstance().getAlliance().equals(DriverStation.Alliance.Blue)
 		? 1.0
 		: -1.0;
-	public static final double DEAD_RECKON_OUTTAKE_TIME = 1.5;
-	public static final double DEAD_RECKON_TIME_BACK_TO_CLEAR_PEG = 1.0;
+	public static final double DEAD_RECKON_OUTTAKE_TIME = 2.5;
+	public static final double DEAD_RECKON_TIME_BACK_TO_CLEAR_PEG = 2.0;
 	public static final double DEAD_RECKON_DRIVE_SPEED = -0.6;
 	public static final double DEAD_RECKON_TURN_SPEED = -0.47;
-	public static final double DEAD_RECKON_WIGGLE_TIME = 1.75; // It's wiggle time!
+	public static final double DEAD_RECKON_WIGGLE_TIME = 1.5; // It's wiggle time!
+	public static final double DEAD_RECKON_OUTTAKE_BACKOFF_DRIVE_SPEED = 0.3;
 }
