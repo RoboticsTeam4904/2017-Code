bc76c80664d3493a450d15120d1fac29b2151a7c
Carter Turnbaugh <carturn@nuevaschool.org>
2017-03-15 17:40:52 -0700

    Make sure camera data is up to date

--- src/org/usfirst/frc4904/sovereignty/strategies/GearAlign.java
+++ src/org/usfirst/frc4904/sovereignty/strategies/GearAlign.java
@@ -5,9 +5,11 @@ import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.standard.LogKitten;
 import org.usfirst.frc4904.standard.commands.KittenCommand;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 public class GearAlign extends CommandGroup {
 	public GearAlign() {
+		addSequential(new WaitCommand(0.25));
 		addSequential(new VisionTurn(RobotMap.Component.gearAlignCamera));
 		addSequential(new KittenCommand("Done gear aligning (I did my best)", LogKitten.LEVEL_WTF));
 	}
