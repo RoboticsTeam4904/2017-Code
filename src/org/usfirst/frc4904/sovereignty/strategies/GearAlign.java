764f6a8f3ae7e84a7212e4a3524d10a4bac52dd8
Yoni Lerner <yonilern@gmail.com>
2017-03-18 17:36:31 -0700

    Added a maximum time to each cycle step

--- src/org/usfirst/frc4904/sovereignty/strategies/GearAlign.java
+++ src/org/usfirst/frc4904/sovereignty/strategies/GearAlign.java
@@ -4,16 +4,18 @@ package org.usfirst.frc4904.sovereignty.strategies;
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.standard.LogKitten;
 import org.usfirst.frc4904.standard.commands.KittenCommand;
+import org.usfirst.frc4904.standard.commands.RunFor;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
 public class GearAlign extends CommandGroup {
 	protected static final int ALIGNMENT_CYCLES = 3;
+	protected static final int MAX_CYCLE_TIME_SECONDS = 1;
 
 	public GearAlign() {
 		for (int i = 0; i < GearAlign.ALIGNMENT_CYCLES; i++) {
 			addSequential(new WaitCommand(0.25));
-			addSequential(new VisionTurn(RobotMap.Component.gearAlignCamera));
+			addSequential(new RunFor(new VisionTurn(RobotMap.Component.gearAlignCamera), GearAlign.MAX_CYCLE_TIME_SECONDS));
 		}
 		addSequential(new KittenCommand("Done gear aligning (I did my best)", LogKitten.LEVEL_WTF));
 	}
