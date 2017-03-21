3bac37607b8d8d42e3f78c6e3e6eba1a42d51512
Yoni Lerner <yonilern@gmail.com>
2017-03-21 11:59:19 -0700

    Restructured to allow for easier addition

--- src/org/usfirst/frc4904/sovereignty/strategies/GearAlign.java
+++ src/org/usfirst/frc4904/sovereignty/strategies/GearAlign.java
@@ -5,6 +5,7 @@ import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.standard.LogKitten;
 import org.usfirst.frc4904.standard.commands.KittenCommand;
 import org.usfirst.frc4904.standard.commands.RunFor;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
@@ -15,8 +16,15 @@ public class GearAlign extends CommandGroup {
 
 	public GearAlign() {
 		for (int i = 0; i < GearAlign.ALIGNMENT_CYCLES; i++) {
-			addSequential(new WaitCommand(GearAlign.INTER_CYCLE_DELAY_TIME_SECONDS));
-			addSequential(new RunFor(new VisionTurn(RobotMap.Component.gearAlignCamera), GearAlign.MAX_CYCLE_TIME_SECONDS));
+			// The pre-turn wait command
+			Command wait = new WaitCommand(GearAlign.INTER_CYCLE_DELAY_TIME_SECONDS);
+			// The actual turn command
+			Command core = new VisionTurn(RobotMap.Component.gearAlignCamera);
+			// Limit the maximum time of the turn command
+			Command timeLimited = new RunFor(core, GearAlign.MAX_CYCLE_TIME_SECONDS);
+			// Actually add all commands
+			addSequential(wait);
+			addSequential(timeLimited);
 		}
 		addSequential(new KittenCommand("Done gear aligning (I did my best)", LogKitten.LEVEL_VERBOSE));
 	}
