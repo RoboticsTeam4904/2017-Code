48cfac4db2e12a635be3578dd6331e6b8edfcd8d
Yoni Lerner <yonilern@gmail.com>
2017-03-21 11:54:44 -0700

    Made inter-cycle delay time a constant

--- src/org/usfirst/frc4904/sovereignty/strategies/GearAlign.java
+++ src/org/usfirst/frc4904/sovereignty/strategies/GearAlign.java
@@ -11,10 +11,11 @@ import edu.wpi.first.wpilibj.command.WaitCommand;
 public class GearAlign extends CommandGroup {
 	protected static final int ALIGNMENT_CYCLES = 3;
 	protected static final int MAX_CYCLE_TIME_SECONDS = 1;
+	protected static final double INTER_CYCLE_DELAY_TIME_SECONDS = 0.25;
 
 	public GearAlign() {
 		for (int i = 0; i < GearAlign.ALIGNMENT_CYCLES; i++) {
-			addSequential(new WaitCommand(0.25));
+			addSequential(new WaitCommand(GearAlign.INTER_CYCLE_DELAY_TIME_SECONDS));
 			addSequential(new RunFor(new VisionTurn(RobotMap.Component.gearAlignCamera), GearAlign.MAX_CYCLE_TIME_SECONDS));
 		}
 		addSequential(new KittenCommand("Done gear aligning (I did my best)", LogKitten.LEVEL_VERBOSE));
