2c78735d8e78d0da7707cbe71f1ff36998c6eaff
Yoni Lerner <yonilern@gmail.com>
2017-03-24 16:49:51 -0700

    Updated pneumatics ports
    Because build insists on changing them every time

--- src/org/usfirst/frc4904/robot/RobotMap.java
+++ src/org/usfirst/frc4904/robot/RobotMap.java
@@ -83,14 +83,14 @@ public class RobotMap {
 		}
 
 		public static class Pneumatics {
-			public static final int shifterUp = 4;
-			public static final int shifterDown = 5;
-			public static final int gearioGullWingsUp = 2;
-			public static final int gearioGullWingsDown = 3;
-			public static final int gearioRampUp = 0;
-			public static final int gearioRampDown = 1;
-			public static final int hopperUp = 6;
-			public static final int hopperDown = 7;
+			public static final int shifterUp = 6;
+			public static final int shifterDown = 7;
+			public static final int gearioGullWingsUp = 0;
+			public static final int gearioGullWingsDown = 1;
+			public static final int gearioRampUp = 2;
+			public static final int gearioRampDown = 3;
+			public static final int hopperUp = 5;
+			public static final int hopperDown = 4;
 		}
 	}
 
