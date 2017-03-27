46c0e249e16fcb254d0dbb122fbad98f24fe9b50
Yoni Lerner <yonilern@gmail.com>
2017-03-27 14:54:53 -0700

    Inverted piston

--- src/org/usfirst/frc4904/robot/subsystems/FloorIO.java
+++ src/org/usfirst/frc4904/robot/subsystems/FloorIO.java
@@ -9,10 +9,10 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class FloorIO extends Subsystem {
 	public final Motor roller;
 	public final DoubleSolenoid piston;
-	public static final DoubleSolenoid.Value LOWERED = DoubleSolenoid.Value.kForward;
-	public static final DoubleSolenoid.Value RAISED = DoubleSolenoid.Value.kReverse;
 	public static final double INTAKE_SPEED = 0.75;
 	public static final double OUTTAKE_SPEED = -0.75;
+	public static final DoubleSolenoid.Value LOWERED = DoubleSolenoid.Value.kReverse;
+	public static final DoubleSolenoid.Value RAISED = DoubleSolenoid.Value.kForward;
 	public static final double OUTTAKE_ROLLER_DELAY_SECONDS = 0.1;
 
 	public FloorIO(Motor roller, DoubleSolenoid piston) {
