15b62f6989c9329790b1ade7241ac9c63bcada27
AJ Nadel <aj@ajnadel.com>
2017-03-30 09:59:42 -0700

    reorganize FloorioClimb

--- src/org/usfirst/frc4904/robot/commands/FloorioClimb.java
+++ src/org/usfirst/frc4904/robot/commands/FloorioClimb.java
@@ -3,14 +3,24 @@ package org.usfirst.frc4904.robot.commands;
 
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.subsystems.FloorIO;
-import org.usfirst.frc4904.standard.commands.SingleOp;
-import org.usfirst.frc4904.standard.commands.WaitWhile;
-import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.Command;
 
-public class FloorioClimb extends CommandGroup {
+/**
+ * Prepare FloorIO for climb and hold it in the LOWERED state.
+ *
+ */
+public class FloorioClimb extends Command {
 	public FloorioClimb() {
 		requires(RobotMap.Component.floorIO);
-		addSequential(new SingleOp(() -> RobotMap.Component.floorIO.piston.set(FloorIO.LOWERED)));
-		addSequential(new WaitWhile(() -> true));
+	}
+
+	@Override
+	protected void initialize() {
+		RobotMap.Component.floorIO.piston.set(FloorIO.LOWERED);
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return false;
 	}
 }
