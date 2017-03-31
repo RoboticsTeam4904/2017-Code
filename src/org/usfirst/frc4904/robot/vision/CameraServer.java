75a71ae8d1aafd06c5d0ae503fe3401b2a5eb6ec
Carter Turnbaugh <carturn@nuevaschool.org>
2017-03-31 13:36:55 -0700

    Switched to UDP sockets

--- src/org/usfirst/frc4904/robot/vision/CameraServer.java
+++ src/org/usfirst/frc4904/robot/vision/CameraServer.java
@@ -2,19 +2,22 @@ package org.usfirst.frc4904.robot.vision;
 
 
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import org.usfirst.frc4904.standard.LogKitten;
 
 public class CameraServer implements Runnable {
 	public final static int CAMERA_PORT = 5802;
-	protected ServerSocket socket;
+	public final static int MAX_CAMERA_PACKET_SIZE = 64;
+	protected DatagramSocket socket;
+	protected DatagramPacket packet;
 	protected double cameraAngle;
 	private final Object lock = new Object();
 
 	public CameraServer() {
 		try {
-			socket = new ServerSocket(CameraServer.CAMERA_PORT);
+			socket = new DatagramSocket(CameraServer.CAMERA_PORT);
+			packet = new DatagramPacket(new byte[CameraServer.MAX_CAMERA_PACKET_SIZE], CameraServer.MAX_CAMERA_PACKET_SIZE);
 		}
 		catch (IOException ex) {
 			LogKitten.ex(ex);
@@ -26,17 +29,10 @@ public class CameraServer implements Runnable {
 		while (true) {
 			try {
 				LogKitten.wtf("Waiting for connection...");
-				Socket client = socket.accept();
-				if (client != null) {
-					StringBuilder newData = new StringBuilder();
-					int next = client.getInputStream().read();
-					while (next != 10 && next > 0) {
-						newData.append((char) next);
-						next = client.getInputStream().read();
-					}
-					client.close();
+				socket.receive(packet);
+				if (packet != null) {
 					synchronized (lock) {
-						cameraAngle = Double.parseDouble(newData.toString());
+						cameraAngle = Double.parseDouble(new String(packet.getData()));
 					}
 					LogKitten.wtf(cameraAngle);
 				}
