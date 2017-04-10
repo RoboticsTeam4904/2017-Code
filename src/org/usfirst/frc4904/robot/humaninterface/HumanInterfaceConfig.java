13dba4b85da7e09f64c2965c1cc0c7a000b693a9
Carter Turnbaugh <carturn@nuevaschool.org>
2017-04-10 12:42:37 -0700

    Removed autoShifter from HumanInterfaceConfig

--- src/org/usfirst/frc4904/robot/humaninterface/HumanInterfaceConfig.java
+++ src/org/usfirst/frc4904/robot/humaninterface/HumanInterfaceConfig.java
@@ -1,12 +1,10 @@
 package org.usfirst.frc4904.robot.humaninterface;
 
 
-import org.usfirst.frc4904.robot.commands.AutoShifter;
 import org.usfirst.frc4904.sovereignty.strategies.GearAlign;
 
 public class HumanInterfaceConfig {
 	public static final GearAlign gearAlign = new GearAlign();
-	public static final AutoShifter autoShifter = new AutoShifter();
 	public static final double XBOX_DEADZONE = 0.1;
 	public static final double JOYSTICK_DEADZONE = 0.1;
 	public static final int TEENSY_STICK_NUM_BUTTONS = 30;
