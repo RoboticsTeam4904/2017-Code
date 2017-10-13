7c4f9e39f4322cffbe7bf242dde960a143d6f8f5
GOAF-Pilot <GOAF-Pilot@users.noreply.github.com>
2017-10-13 16:56:03 -0700

    add javadoc comments

--- src/org/usfirst/frc4904/robot/commands/Climb.java
+++ src/org/usfirst/frc4904/robot/commands/Climb.java
@@ -1,32 +1,49 @@
 package org.usfirst.frc4904.robot.commands;
 
-
 import java.util.function.Supplier;
+
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.subsystems.GearIO;
 import org.usfirst.frc4904.standard.commands.motor.MotorSet;
+
 import edu.wpi.first.wpilibj.command.Command;
 
+/**
+ * sets the Climb motor speed to the joystick input value
+ *
+ */
+// TODO: @mickey clean this up
 public class Climb extends MotorSet {
 	protected final Supplier<Double> axis;
 	protected final Command rampCommand = new RampSet(GearIO.RampState.RETRACTED);
 	protected final Command floorioCommand = new FloorioClimb();
 	protected static final double MINIMUM_AXIS_VALUE_TO_FLIP_GEARIO_RAMP = 0.25;
 
+	/**
+	 * 
+	 * @param axis
+	 *            is a joystick control axis for climbing
+	 */
 	public Climb(Supplier<Double> axis) {
-		super(RobotMap.Component.climber);
+		super(RobotMap.Component.climber);// gives MotorSet (Parent class) the
+											// climber motor
 		this.axis = axis;
 	}
 
 	@Override
-	protected void initialize() {}
+	protected void initialize() {
+	}
 
+	/**
+	 * Start rampCommand if not started, start floorioCommand if not started.
+	 * Run the climb motor at the speed from the joystick.
+	 */
 	@Override
 	protected void execute() {
 		set(Math.max(0, axis.get())); // No, really, always feed positive values
 										// to the climber motor
 		if (axis.get() >= Climb.MINIMUM_AXIS_VALUE_TO_FLIP_GEARIO_RAMP && RobotMap.Component.gearIO.isNotOverridden()
-			&& !rampCommand.isRunning()) {
+				&& !rampCommand.isRunning()) {
 			rampCommand.start();
 		}
 		if (axis.get() > 0 && !floorioCommand.isRunning()) {
