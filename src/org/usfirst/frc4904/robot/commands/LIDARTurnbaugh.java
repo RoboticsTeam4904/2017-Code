fb0f95e1e99c11b8b02acb2412659e180545cbee
GOAF-Pilot <GOAF-Pilot@users.noreply.github.com>
2017-10-13 17:02:15 -0700

    add javadoc comments

--- src/org/usfirst/frc4904/robot/commands/LIDARTurnbaugh.java
+++ src/org/usfirst/frc4904/robot/commands/LIDARTurnbaugh.java
@@ -1,23 +1,27 @@
 package org.usfirst.frc4904.robot.commands;
 
-
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.subsystems.LIDAR;
 import org.usfirst.frc4904.standard.LogKitten;
 import org.usfirst.frc4904.standard.commands.motor.MotorConstant;
 import org.usfirst.frc4904.standard.custom.sensors.InvalidSensorException;
 
+/**
+ * Initializes the LIDAR and enables it to be controlled.
+ *
+ */
+
 public class LIDARTurnbaugh extends MotorConstant {
 	public LIDARTurnbaugh() {
-		super(RobotMap.Component.lidar, LIDAR.TARGET_MRPM);
+		super(RobotMap.Component.lidar, LIDAR.TARGET_MRPM);// set the LIDAR
+															// motor and speed
 	}
 
 	@Override
 	protected void initialize() {
 		try {
 			RobotMap.Component.lidar.reset();
-		}
-		catch (InvalidSensorException e) {
+		} catch (InvalidSensorException e) {
 			LogKitten.e("LIDAR error!");
 			LogKitten.e(e);
 			LogKitten.e(e.getStackTrace());
