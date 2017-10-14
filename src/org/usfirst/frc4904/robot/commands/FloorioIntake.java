779a166744b52122055b41948bcb6f40d800cf3d
Quincy Alston <quialst@nuevaschool.org>
2017-10-13 17:42:06 -0700

    Added documentation to FloorioIntake

--- src/org/usfirst/frc4904/robot/commands/FloorioIntake.java
+++ src/org/usfirst/frc4904/robot/commands/FloorioIntake.java
@@ -1,12 +1,16 @@
 package org.usfirst.frc4904.robot.commands;
 
-
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.subsystems.FloorIO;
 import org.usfirst.frc4904.standard.commands.SingleOp;
 import org.usfirst.frc4904.standard.commands.motor.MotorConstant;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
+/**
+ * Runs the Floorio Intake motor at a constant speed.
+ *
+ */
 public class FloorioIntake extends CommandGroup {
 	public FloorioIntake() {
 		requires(RobotMap.Component.floorIO);
@@ -14,6 +18,9 @@ public class FloorioIntake extends CommandGroup {
 		addParallel(new SingleOp(() -> RobotMap.Component.floorIO.piston.set(FloorIO.LOWERED)));
 	}
 
+	/**
+	 * Secures gear into Floorio
+	 */
 	@Override
 	protected void interrupted() {
 		new FloorioSecureGear().start();
