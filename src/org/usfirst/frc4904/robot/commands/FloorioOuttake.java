c37d716a7f755d98ef2b2297b55b19cefdfb6b93
Quincy Alston <quialst@nuevaschool.org>
2017-10-13 18:00:10 -0700

    Added documentation to FloorioOuttake

--- src/org/usfirst/frc4904/robot/commands/FloorioOuttake.java
+++ src/org/usfirst/frc4904/robot/commands/FloorioOuttake.java
@@ -1,16 +1,22 @@
 package org.usfirst.frc4904.robot.commands;
 
-
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.subsystems.FloorIO;
 import org.usfirst.frc4904.standard.commands.SingleOp;
 import org.usfirst.frc4904.standard.commands.motor.MotorConstant;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
+/**
+ * Runs the Floorio outtake motor at a constant speed Sequentially delays the
+ * roller outtake Add a parallel outtake motor constant
+ * 
+ */
 public class FloorioOuttake extends CommandGroup {
 	public FloorioOuttake() {
 		requires(RobotMap.Component.floorIO);
+		// TODO: @mickey
 		addParallel(new SingleOp(() -> RobotMap.Component.floorIO.piston.set(FloorIO.LOWERED)));
 		addSequential(new WaitCommand(FloorIO.OUTTAKE_ROLLER_DELAY_SEC));
 		addParallel(new MotorConstant(RobotMap.Component.floorIO.roller, FloorIO.OUTTAKE_SPEED));
