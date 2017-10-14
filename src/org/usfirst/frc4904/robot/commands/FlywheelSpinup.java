83a0fe85bdd452d364cbf5c5079713a91501736f
Lauren W <lauwong@nuevaschool.org>
2017-10-13 17:29:36 -0700

    Added java doc to flywheel spinup

--- src/org/usfirst/frc4904/robot/commands/FlywheelSpinup.java
+++ src/org/usfirst/frc4904/robot/commands/FlywheelSpinup.java
@@ -1,9 +1,12 @@
 package org.usfirst.frc4904.robot.commands;
 
-
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.subsystems.Flywheel;
 import org.usfirst.frc4904.standard.commands.motor.MotorSet;
+
+/**
+ * Sets Flywheel speed to constant SHOOTING_SPEED
+ **/
 
 public class FlywheelSpinup extends MotorSet {
 	public FlywheelSpinup() {
