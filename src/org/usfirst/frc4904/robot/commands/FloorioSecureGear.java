b5a970d781686b2afebe0d85982d963b358294b0
Quincy Alston <quialst@nuevaschool.org>
2017-10-13 18:11:51 -0700

    Added documentation to FloorioSecureGear

--- src/org/usfirst/frc4904/robot/commands/FloorioSecureGear.java
+++ src/org/usfirst/frc4904/robot/commands/FloorioSecureGear.java
@@ -1,12 +1,16 @@
 package org.usfirst.frc4904.robot.commands;
 
-
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.subsystems.FloorIO;
 import org.usfirst.frc4904.standard.commands.SingleOp;
 import org.usfirst.frc4904.standard.commands.motor.MotorConstant;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
+/**
+ * Raises Floorio and sets Floorio motor at constant intake speed
+ *
+ */
 public class FloorioSecureGear extends CommandGroup {
 	public FloorioSecureGear() {
 		setTimeout(FloorIO.SECURE_GEAR_TIME_SEC);
